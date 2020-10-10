5535d908064ff9215e9589fd2d511c0eca7d0735
Devon Reid <fireborn1003@gmail.com>
2020-10-10 10:34:38 +0600

    FEAT: Added collection property update function

--- api/Collection/index.js
+++ api/Collection/index.js
@@ -2,14 +2,11 @@ const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
 
 const Transaction = require('../transaction');
-const Collection = require('../Collection');
 
-const { blockUpdate, blockSet } = require('../Block/utils');
+const { collectionSet } = require('../Collection/utils');
+const pluckKeys = require('../../utils/pluckKeys');
 
-const { collectionUpdate } = require('../Collection/utils');
-const { collectionViewUpdate } = require('../CollectionView/utils');
-
-module.exports = class Collection {
+class Collection {
 	static setStatic (obj) {
 		Object.entries(obj).forEach(([ key, value ]) => (Collection[key] = value));
 		return Collection;
@@ -19,5 +16,25 @@ module.exports = class Collection {
 		Object.entries(obj).forEach(([ key, value ]) => (this[key] = value));
 	}
 
-	static async getCollection () {}
-};
+	async updateProperties (properties) {
+		const property_entries = Object.entries(
+			pluckKeys(properties, this.collection_data, [
+				[ 'name', (data) => [ [ data ] ] ],
+				[ 'description', (data) => [ [ data ] ] ],
+				'icon'
+			])
+		);
+		await axios.post(
+			'https://www.notion.so/api/v3/saveTransactions',
+			Transaction.createTransaction([
+				[
+					...property_entries.map(([ path, arg ]) => collectionSet(this.collection_data.id, [ path ], arg)),
+					collectionSet(this.collection_data.id, [ 'last_edited_time' ], Date.now())
+				]
+			]),
+			Collection.headers
+		);
+	}
+}
+
+module.exports = Collection;

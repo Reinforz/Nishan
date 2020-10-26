34ad2602cc8120cb940cd85d68ba2e476e779f46
Devon Reid <fireborn1003@gmail.com>
2020-10-26 20:22:47 +0600

    FEAT: Added SpaceView.getSpace method

--- api/SpaceView.ts
+++ api/SpaceView.ts
@@ -1,8 +1,9 @@
 import Data from './Data';
 
 import { NishanArg } from '../types/types';
-import { ISpaceView } from '../types/api';
+import { ISpace, ISpaceView } from '../types/api';
 import { UpdatableSpaceViewParam } from '../types/function';
+import Space from './Space';
 
 class SpaceView extends Data<ISpaceView> {
   constructor(arg: NishanArg<ISpaceView>) {
@@ -17,6 +18,22 @@ class SpaceView extends Data<ISpaceView> {
       op
     ]);
     update();
+  }
+
+  async getSpace() {
+    let target_space: ISpace | null = null;
+    for (let [, space] of this.cache.space) {
+      if (this.data && space.id === this.data.space_id) {
+        target_space = space;
+        break;
+      }
+    }
+    if (target_space)
+      return new Space({
+        type: "space",
+        data: target_space,
+        ...this.getProps()
+      });
   }
 }
 

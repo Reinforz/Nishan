4f276ccd47d5a924da30bf3be09d31987f9f07dc
Devon Reid <fireborn1003@gmail.com>
2020-10-28 20:02:46 +0600

    DOCS: Added CollectionView and Page brief desc

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,6 +1,11 @@
 module.exports = {
 	someSidebar: {
-		Introduction: [ 'Introduction/Getting Started', 'Introduction/Understanding Nishan', 'Introduction/Features' ],
+		Introduction: [
+			'Introduction/Getting Started',
+			'Introduction/Features',
+			'Introduction/Understanding Nishan',
+			'Introduction/Understanding Notion'
+		],
 		API: require('./typedoc-sidebars')
 	}
 };

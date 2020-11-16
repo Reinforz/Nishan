89f7015c74d6ddc512c18d3998669679309bc726
Safwan Shaheer <fireborn1003@gmail.com>
2020-11-16 19:23:27 +0600

    FEAT: Added [ViewSchemaUnit].update method

--- api/ViewSchemaUnit.ts
+++ api/ViewSchemaUnit.ts
@@ -10,7 +10,18 @@ export default class ViewSchemaUnit extends Data<TView> {
   schema_data: ViewFormatProperties;
 
   constructor(arg: NishanArg & { schema_data: ViewFormatProperties }) {
-    super({ ...arg, type: "collection" });
+    super({ ...arg, type: "collection_view" });
     this.schema_data = arg.schema_data
+  }
+
+  async update(arg: Partial<Omit<ViewFormatProperties, "property">>) {
+    const data = this.getCachedData();
+    this.saveTransactions([this.updateOp([], {
+      format: {
+        ...data.format,
+        [`${data.type}_properties`]: (data.format[`${data.type}_properties` as never] as ViewFormatProperties[]).map(properties => properties.property === this.schema_data.property ? { ...properties, ...arg } : properties)
+      }
+    })])
+    this.updateCacheManually([this.id]);
   }
 }

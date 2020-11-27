457de8e0d7a1ebea7d67cfd32ba04c79d65ab8ab
Safwan Shaheer <fireborn1003@gmail.com>
2020-11-27 13:18:22 +0600

    FEAT: Added category options for workflow_2

--- workflows/workflow_2/data/options.ts
+++ workflows/workflow_2/data/options.ts
@@ -3,6 +3,10 @@ import {
 } from 'uuid';
 import { SelectOption, TTextColor } from '../../../types';
 
-const options: [string, TTextColor][] = [["Javascript", "yellow"], ["Python", "blue"], ["C#", "purple"], ["Dart", "blue"], ["Node", "teal"], ["PHP", "purple"], ["CSS", "blue"]];
+const language_options: [string, TTextColor][] = [["Javascript", "yellow"], ["Python", "blue"], ["C#", "purple"], ["Dart", "blue"], ["Node", "green"], ["PHP", "purple"], ["CSS", "blue"]];
+const category_options: [string, TTextColor][] = [["Database", "green"], ["Framework", "orange"], ["Language", "purple"], ["Library", "brown"], ["Tools", "default"], ["Technology", "pink"]];
 
-export const LanguageOptions = options.map(([value, color]) => ({ id: uuidv4(), value, color })) as SelectOption[]
+export default {
+  language: language_options.map(([value, color]) => ({ id: uuidv4(), value, color })) as SelectOption[],
+  category: category_options.map(([value, color]) => ({ id: uuidv4(), value, color })) as SelectOption[],
+} 

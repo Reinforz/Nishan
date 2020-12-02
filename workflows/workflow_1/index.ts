7db68fd80af20a9906fd51609f5a24f8f29835f0
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-02 20:45:21 +0600

    FEAT: Completed Article cvp schema creation

--- workflows/workflow_1/index.ts
+++ workflows/workflow_1/index.ts
@@ -1,5 +1,7 @@
 import Nishan from '../../Nishan';
 import priority from '../data/priority';
+import status from '../data/status';
+import phase from '../data/phase';
 
 import "../env"
 
@@ -21,33 +23,70 @@ import "../env"
       page_full_width: true,
       page_icon: "https://notion-emojis.s3-us-west-2.amazonaws.com/v0/svg-twitter/1f4d4.svg"
     },
-    schema: [{ type: "title", name: "Title" }, {
-      type: "formula",
-      name: "Urgency"
-    },
-    {
-      type: "formula",
-      name: "Completed"
-    },
-    {
-      type: "multi_select",
-      name: "Subject",
-      options: []
-    },
-    {
-      type: "multi_select",
-      name: "Provider",
-      options: []
-    },
-    {
-      type: "url",
-      name: "Source"
-    },
-    {
-      type: "select",
-      name: "Priority",
-      options: priority
-    }],
+    schema: [
+      { type: "title", name: "Title" },
+      {
+        type: "formula",
+        name: "Urgency"
+      },
+      {
+        type: "formula",
+        name: "Completed"
+      },
+      {
+        type: "multi_select",
+        name: "Subject",
+        options: []
+      },
+      {
+        type: "multi_select",
+        name: "Provider",
+        options: []
+      },
+      {
+        type: "url",
+        name: "Source"
+      },
+      {
+        type: "select",
+        name: "Priority",
+        options: priority
+      },
+      {
+        type: "select",
+        name: "Status",
+        options: status
+      },
+      {
+        type: "select",
+        name: "Phase",
+        options: phase
+      },
+      {
+        type: "date",
+        name: "Learn Range",
+      },
+      {
+        type: "date",
+        name: "Revise Range",
+      },
+      {
+        type: "date",
+        name: "Practice Range",
+      },
+      {
+        type: "formula",
+        name: "Priority Counter",
+      },
+      {
+        type: "formula",
+        name: "Status Counter",
+      },
+      {
+        type: "formula",
+        name: "Phase Counter",
+      }
+    ],
     views: [
       {
         name: "Left Articles",

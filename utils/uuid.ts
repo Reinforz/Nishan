0c89cd75e283d516c2898be0e4f2b2793b353261
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-13 18:45:01 +0600

    FEAT: Added uuid-id and id-uuid conversion utils

--- utils/uuid.ts
+++ utils/uuid.ts
@@ -3,3 +3,12 @@ export function validateUUID(uuid: string): boolean {
   const REGEX = /^(?:[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}|00000000-0000-0000-0000-000000000000)$/i;
   return typeof uuid === 'string' && REGEX.test(uuid);
 }
+
+/* Credit goes to @transitive-bullshit https://github.com/transitive-bullshit*/
+export const idToUuid = (id: string = '') =>
+  `${id.substr(0, 8)}-${id.substr(8, 4)}-${id.substr(12, 4)}-${id.substr(
+    16,
+    4
+  )}-${id.substr(20)}`
+
+export const uuidToId = (uuid: string) => uuid.replace(/-/g, '')

616737f54c263b75b267a3dc078a4e4518401ea1
Safwan Shaheer <fireborn1003@gmail.com>
2020-12-31 12:14:00 +0600

    RF: Added util method for mutation wrapper

--- api/Mutations.ts
+++ api/Mutations.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { Request, RemoveUsersFromSpaceResult, RemoveUsersFromSpaceParams, IOperation, NishanArg, SetPageNotificationsAsReadParams, SetSpaceNotificationsAsReadParams, CreateSpaceParams, CreateSpaceResult, EnqueueTaskResult, InviteGuestsToSpaceParams, RecordMap, SetBookmarkMetadataParams, TEnqueueTaskParams } from "../types";
+import { Request, RemoveUsersFromSpaceResult, RemoveUsersFromSpaceParams, IOperation, NishanArg, SetPageNotificationsAsReadParams, SetSpaceNotificationsAsReadParams, CreateSpaceParams, CreateSpaceResult, EnqueueTaskResult, InviteGuestsToSpaceParams, SetBookmarkMetadataParams, TEnqueueTaskParams } from "../types";
 import { createTransaction, error } from "../utils";
 import Queries from "./Queries";
 
@@ -15,141 +15,62 @@ export default class Mutations extends Queries {
     this.createTransaction = createTransaction.bind(this, shard_id, space_id);
   }
 
-  protected async setPageNotificationsAsRead(arg: SetPageNotificationsAsReadParams) {
+  #returnPromise = <T>(url: string, arg: any): Promise<T> => {
     return new Promise((resolve, reject) => {
       setTimeout(async () => {
         try {
-          await axios.post(
-            `${this.BASE_NOTION_URL}/setPageNotificationsAsRead`,
+          const { data } = await axios.post<T>(
+            `${this.BASE_NOTION_URL}/${url}`,
             arg,
             this.headers
           );
+          resolve(data)
         } catch (err) {
           reject(error(err.response.data))
         }
       }, this.interval)
     });
+  }
+
+  protected async setPageNotificationsAsRead(arg: SetPageNotificationsAsReadParams) {
+    return this.#returnPromise('setPageNotificationsAsRead', arg);
   }
 
   protected async setSpaceNotificationsAsRead(arg: SetSpaceNotificationsAsReadParams) {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          await axios.post(
-            `${this.BASE_NOTION_URL}/setSpaceNotificationsAsRead`,
-            arg,
-            this.headers
-          );
-        } catch (err) {
-          reject(error(err.response.data))
-        }
-      }, this.interval)
-    });
+    return this.#returnPromise('setSpaceNotificationsAsRead', arg)
   }
 
-  protected async removeUsersFromSpace(arg: RemoveUsersFromSpaceParams): Promise<RemoveUsersFromSpaceResult> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const { data } = await axios.post(
-            `${this.BASE_NOTION_URL}/removeUsersFromSpace`,
-            arg,
-            this.headers
-          ) as { data: RemoveUsersFromSpaceResult };
-          this.saveToCache(data.recordMap);
-          resolve(data);
-        } catch (err) {
-          reject(error(err.response.data))
-        }
-      }, this.interval)
-    });
+  protected async removeUsersFromSpace(arg: RemoveUsersFromSpaceParams) {
+    const response = await this.#returnPromise<RemoveUsersFromSpaceResult>('removeUsersFromSpace', arg)
+    this.saveToCache(response.recordMap);
+    return response;
   }
 
-  protected async inviteGuestsToSpace(arg: InviteGuestsToSpaceParams): Promise<void> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          await axios.post(
-            `${this.BASE_NOTION_URL}/inviteGuestsToSpace`,
-            arg,
-            this.headers
-          );
-          resolve()
-        } catch (err) {
-          reject(error(err.response.data))
-        }
-      }, this.interval)
+  protected async inviteGuestsToSpace(arg: InviteGuestsToSpaceParams) {
+    return this.#returnPromise('inviteGuestsToSpace', arg);
+  }
+
+  protected async createSpace(arg: Partial<CreateSpaceParams>): Promise<CreateSpaceResult> {
+    const response = await this.#returnPromise<CreateSpaceResult>('createSpace', {
+      ...arg,
+      planType: "personal",
+      initialUseCases: []
     })
+    this.saveToCache(response.recordMap);
+    return response;
   }
 
-  protected async createSpace(params: Partial<CreateSpaceParams>): Promise<CreateSpaceResult> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const { data, data: { recordMap } } = await axios.post(
-            `${this.BASE_NOTION_URL}/createSpace`,
-            {
-              ...params,
-              planType: "personal",
-              initialUseCases: []
-            },
-            this.headers
-          ) as { data: CreateSpaceResult };
-          this.saveToCache(recordMap);
-          resolve(data);
-        } catch (err) {
-          reject(error(err.response.data))
-        }
-      }, this.interval)
-    })
-  }
-
-  protected async saveTransactions(Operations: IOperation[]): Promise<RecordMap> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const res = await axios.post(`${this.BASE_NOTION_URL}/saveTransactions`, this.createTransaction(Operations), this.headers);
-          resolve(res.data.recordMap);
-        } catch (err) {
-          reject(error(err.response.data));
-        }
-      }, this.interval)
-    })
+  protected async saveTransactions(Operations: IOperation[]) {
+    return this.#returnPromise('saveTransactions', this.createTransaction(Operations));
   }
 
   // ? TD:2:M Add task typedef
   // ? TD:2:M Add EnqueueTaskResult interface
-  protected async enqueueTask(task: TEnqueueTaskParams): Promise<EnqueueTaskResult> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          const res = await axios.post(
-            `${this.BASE_NOTION_URL}/enqueueTask`, {
-            task
-          }, this.headers);
-          resolve(res.data);
-        } catch (err) {
-          reject(error(err.response.data));
-        }
-      }, this.interval)
-    })
+  protected async enqueueTask(task: TEnqueueTaskParams) {
+    return this.#returnPromise<EnqueueTaskResult>('enqueueTask', task);
   }
 
-  protected async setBookmarkMetadata(arg: SetBookmarkMetadataParams): Promise<undefined> {
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        try {
-          await axios.post(
-            `${this.BASE_NOTION_URL}/setBookmarkMetadata`,
-            arg,
-            this.headers
-          );
-          resolve(undefined);
-        } catch (err) {
-          reject(error(err.response.data))
-        }
-      })
-    })
+  protected async setBookmarkMetadata(arg: SetBookmarkMetadataParams) {
+    return this.#returnPromise('setBookmarkMetadata', arg);
   }
-
 }

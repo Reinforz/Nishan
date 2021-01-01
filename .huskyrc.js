a10cbfee8148b72f8df2208516fa8b09372d909c
Safwan Shaheer <fireborn1003@gmail.com>
2021-01-01 23:07:36 +0600

    RF: Initial conversion to monorepo in core package

--- .huskyrc.js
+++ .huskyrc.js
@@ -1,6 +1,6 @@
 module.exports = {
 	hooks: {
-		'pre-commit': 'npm run typecheck && npm run lint && npm run test',
-		'pre-push': 'npm run lint && npm run test'
+		/* 'pre-commit': 'npm run typecheck && npm run lint && npm run test',
+		'pre-push': 'npm run lint && npm run test' */
 	}
 };

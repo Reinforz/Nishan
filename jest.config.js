fc66c5bf0c4ca4c231c38c1855698dda44b732ad
Safwan Shaheer <fireborn1003@gmail.com>
2021-01-01 16:51:59 +0600

    TEST: Added dummy test and jest.config.js

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	testTimeout: 30000,
+	testEnvironment: 'node',
+	verbose: true,
+	testPathIgnorePatterns: [ '<rootDir>/node_modules', '<rootDir>/dist' ],
+	modulePathIgnorePatterns: [ '<rootDir>/dist' ],
+	roots: [ '<rootDir>/tests' ],
+	testMatch: [ '<rootDir>/tests/**/*.ts' ],
+	transform: {
+		'^.+\\.(ts)$': 'ts-jest'
+	}
+};
---
+++ tests/success.test.ts
@@ -0,0 +1,3 @@
+it("Passes the test hopefully", () => {
+  expect(2).toEqual(2)
+})

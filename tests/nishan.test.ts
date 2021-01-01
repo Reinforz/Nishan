06b299ad53e4d2119f112ccbb8be24f203b41224
Safwan Shaheer <fireborn1003@gmail.com>
2021-01-01 20:38:27 +0600

    RF+TEST: Better util method to test result and notionuser tests

--- tests/nishan.test.ts
+++ tests/nishan.test.ts
@@ -11,10 +11,28 @@ nishan.saveToCache(data.recordMap)
 
 const USER_ONE_ID = "d94caf87-a207-45c3-b3d5-03d157b5b39b";
 
-function checkUser(user: NotionUser){
-  expect(user).not.toBeNull();
-  expect(user.id).toBe(USER_ONE_ID);
-  expect(user.type).toBe("notion_user");
+function checkUser(user: NotionUser, status?: boolean){
+  status = status ?? true;
+  if(status){
+    expect(user).not.toBeNull();
+    expect(user.id).toBe(USER_ONE_ID);
+    expect(user.type).toBe("notion_user");
+  }else{
+    expect(user).toBeUndefined();
+  }
+}
+
+function checkUsers(users: NotionUser[], status?:boolean){
+  status = status ?? true;
+  if(status){
+    expect(users.length).toBe(1);
+    expect(users[0]).not.toBeNull();
+    expect(users[0].id).toBe(USER_ONE_ID);
+    expect(users[0].type).toBe("notion_user");
+  }else{
+    expect(users.length).toBe(0);
+    expect(users[0]).toBeUndefined();
+  }
 }
 
 it("Sets up default configuration for Nishan",()=>{
@@ -22,50 +40,38 @@ it("Sets up default configuration for Nishan",()=>{
   expect(nishan.interval).toBe(500);
 })
 
-it("Gets the correct notion user when passed a correct id", async ()=>{
-  const user = await nishan.getNotionUser(USER_ONE_ID);
-  checkUser(user)
+it("Get notion_user id", async ()=>{
+  checkUser(await nishan.getNotionUser(USER_ONE_ID))
 })
 
-it("Doesnt get the notion user when passed a wrong id", async ()=>{
-  const user = await nishan.getNotionUser(USER_ONE_ID.slice(1));
-  expect(user).toBeUndefined();
+it("!Get notion_user !id", async ()=>{
+  checkUser(await nishan.getNotionUser(USER_ONE_ID.slice(1)), false)
 })
 
-it("Gets the notion users when passed an array of correct id", async ()=>{
-  const users = await nishan.getNotionUsers([USER_ONE_ID]);
-  expect(users.length).toBe(1);
-  checkUser(users[0]);
+it("Get [notion_user] [id]", async ()=>{
+  checkUsers(await nishan.getNotionUsers([USER_ONE_ID]));
 })
 
-it("Doesnt get the notion users when passed an array of wrong id", async ()=>{
-  const users = await nishan.getNotionUsers([USER_ONE_ID.slice(1)]);
-  expect(users.length).toBe(0);
-  expect(users[0]).toBeUndefined();
+it("!Get [notion_user] ![id]", async ()=>{
+  checkUsers( await nishan.getNotionUsers([USER_ONE_ID.slice(1)]), false)
 })
 
-it("Gets the notion user when passed a correct callback", async ()=>{
-  const user = await nishan.getNotionUser((user)=>user.id === USER_ONE_ID);
-  checkUser(user);
+it("Get notion_user cb", async ()=>{
+  checkUser(await nishan.getNotionUser((user)=>user.id === USER_ONE_ID));
 })
 
-it("Doesnt get the notion user when passed a wrong callback", async ()=>{
-  const user = await nishan.getNotionUser((user)=>user.id === USER_ONE_ID.slice(1));
-  expect(user).toBeUndefined();
+it("!Get notion_user !cb", async ()=>{
+  checkUser(await nishan.getNotionUser((user)=>user.id === USER_ONE_ID.slice(1)), false);
 })
 
-it("Gets the notion users when passed a correct callback", async ()=>{
-  const users = await nishan.getNotionUsers((user)=>user.id === USER_ONE_ID);
-  checkUser(users[0]);
+it("Get [notion_user] cb", async ()=>{
+  checkUsers(await nishan.getNotionUsers((user)=>user.id === USER_ONE_ID));
 })
 
-it("Gets all the notion users when passed undefined", async ()=>{
-  const users = await nishan.getNotionUsers();
-  checkUser(users[0]);
+it("Get [notion_user] undefined", async ()=>{
+  checkUsers(await nishan.getNotionUsers());
 })
 
-it("Doesnt get the notion users when passed a wrong callback", async ()=>{
-  const users = await nishan.getNotionUsers((user)=>user.id === USER_ONE_ID.slice(1));
-  expect(users.length).toBe(0);
-  expect(users[0]).toBeUndefined();
+it("!Get [notion_user] !cb", async ()=>{
+  checkUsers(await nishan.getNotionUsers((user)=>user.id === USER_ONE_ID.slice(1)), false)
 })
--- tests/notionuser.test.ts
+++ tests/notionuser.test.ts
@@ -1,4 +1,4 @@
-import Nishan, { Space } from "../dist/Nishan";
+import Nishan, { NotionUser, Space } from "../dist/Nishan";
 import data from "./data";
 
 const nishan = new Nishan({
@@ -11,15 +11,44 @@ nishan.saveToCache(data.recordMap)
 
 const USER_ONE_ID = "d94caf87-a207-45c3-b3d5-03d157b5b39b",
   SPACE_ONE_ID = "d2498a62-99ed-4ffd-b56d-e986001729f4";
+let user: NotionUser = null as any;
 
-function checkSpace(space: Space){
-  expect(space).not.toBeNull();
-  expect(space.id).toBe(SPACE_ONE_ID);
-  expect(space.type).toBe("space");
+beforeAll(async ()=>{
+  user = await nishan.getNotionUser(USER_ONE_ID)
+})
+
+function checkSpace(space: Space, status?: boolean){
+  status = status ?? true;
+  if(status){
+    expect(space).not.toBeNull();
+    expect(space.id).toBe(SPACE_ONE_ID);
+    expect(space.type).toBe("space");
+  }else{
+    expect(space).toBeUndefined();
+  }
 }
 
-it("Gets the correct space when passed a correct id", async ()=>{
-  const user = await nishan.getNotionUser(USER_ONE_ID);
-  const space = await user.getSpace(SPACE_ONE_ID);
-  checkSpace(space)
+function checkSpaces(spaces: Space[], status?:boolean){
+  status = status ?? true;
+  if(status){
+    expect(spaces.length).toBe(1);
+    expect(spaces[0]).not.toBeNull();
+    expect(spaces[0].id).toBe(SPACE_ONE_ID);
+    expect(spaces[0].type).toBe("space");
+  }else{
+    expect(spaces.length).toBe(0);
+    expect(spaces[0]).toBeUndefined();
+  }
+}
+
+it("Get space id", async ()=>{
+  checkSpace(await user.getSpace(SPACE_ONE_ID))
+})
+
+it("!Get space !id", async ()=>{
+  checkSpace(await user.getSpace(SPACE_ONE_ID.slice(1)), false)
+})
+
+it("Get [space] [ids]", async ()=>{
+  checkSpaces(await user.getSpaces([SPACE_ONE_ID]));
 })

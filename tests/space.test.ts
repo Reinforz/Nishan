ad2fe5d3ea9769ac5c7837ea9d25c8d8e23e109b
Safwan Shaheer <fireborn1003@gmail.com>
2021-01-01 21:57:20 +0600

    TEST: Added tests for space.cvppage(s) getter methods

--- tests/space.test.ts
+++ tests/space.test.ts
@@ -1,6 +1,6 @@
 import { Space } from "../dist/api";
 import { ITPage } from "../dist/types";
-import {nishan, USER_ONE_ID, SPACE_ONE_ID, ROOT_PAGE_ONE_ID} from "./constants"
+import {nishan, ROOT_COLLECTION_VIEW_PAGE_ONE_ID, USER_ONE_ID, SPACE_ONE_ID, ROOT_PAGE_ONE_ID} from "./constants"
 
 let space: Space = null as any;
 
@@ -19,6 +19,19 @@ function checkRootPages(pages: ITPage, status?:boolean){
   }else{
     expect(pages.page.length).toBe(0);
     expect(pages.page[0]).toBeUndefined();
+  }
+}
+
+function checkRootCollectionViewPages(pages: ITPage, status?:boolean){
+  status = status ?? true;
+  if(status){
+    expect(pages.collection_view_page.length).toBe(1);
+    expect(pages.collection_view_page[0]).not.toBeNull();
+    expect(pages.collection_view_page[0].id).toBe(ROOT_COLLECTION_VIEW_PAGE_ONE_ID);
+    expect(pages.collection_view_page[0].type).toBe("block");
+  }else{
+    expect(pages.collection_view_page.length).toBe(0);
+    expect(pages.collection_view_page[0]).toBeUndefined();
   }
 }
 
@@ -46,8 +59,12 @@ it("!Get root_page !cb", async ()=>{
   checkRootPages(await space.getTRootPage(root_page=>root_page.id === ROOT_PAGE_ONE_ID.slice(1)), false);
 })
 
-it("Get [root_page] cb", async ()=>{
+it("Get [root_page] cb.id", async ()=>{
   checkRootPages(await space.getTRootPages(root_page=>root_page.id === ROOT_PAGE_ONE_ID));
+})
+
+it("Get [root_page] cb.type", async ()=>{
+  checkRootPages(await space.getTRootPages(root_page=>root_page.type === "page"));
 })
 
 it("Get [root_page] undefined", async ()=>{
@@ -56,4 +73,45 @@ it("Get [root_page] undefined", async ()=>{
 
 it("!Get [root_page] !cb", async ()=>{
   checkRootPages(await space.getTRootPages(root_page=>root_page.id === ROOT_PAGE_ONE_ID.slice(1)), false);
+})
+
+
+it("Get root_cvp id", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPage(ROOT_COLLECTION_VIEW_PAGE_ONE_ID))
+})
+
+it("!Get root_cvp !id", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPage(ROOT_COLLECTION_VIEW_PAGE_ONE_ID.slice(1)), false)
+})
+
+it("Get [root_cvp] [id]", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPages([ROOT_COLLECTION_VIEW_PAGE_ONE_ID]));
+})
+
+it("!Get [root_cvp] ![id]", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPages([ROOT_COLLECTION_VIEW_PAGE_ONE_ID.slice(1)]), false);
+})
+
+it("Get root_cvp cb", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPage(root_cvp=>root_cvp.id === ROOT_COLLECTION_VIEW_PAGE_ONE_ID));
+})
+
+it("!Get root_cvp !cb", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPage(root_cvp=>root_cvp.id === ROOT_COLLECTION_VIEW_PAGE_ONE_ID.slice(1)), false);
+})
+
+it("Get [root_cvp] cb.id", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPages(root_cvp=>root_cvp.id === ROOT_COLLECTION_VIEW_PAGE_ONE_ID));
+})
+
+it("Get [root_cvp] cb.type", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPages(root_cvp=>root_cvp.type === "collection_view_page"));
+})
+
+it("Get [root_cvp] undefined", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPages());
+})
+
+it("!Get [root_cvp] !cb", async ()=>{
+  checkRootCollectionViewPages(await space.getTRootPages(root_cvp=>root_cvp.id === ROOT_COLLECTION_VIEW_PAGE_ONE_ID.slice(1)), false);
 })

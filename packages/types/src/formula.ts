0f8d54da27fadc4e050ea4f723dabc5a98814d74
Safwan Shaheer <devorein00@gmail.com>
2021-01-05 16:00:21 +0600

    TD: Added property based formula tds

--- packages/types/src/formula.ts
+++ packages/types/src/formula.ts
@@ -1,32 +1,59 @@
+export type TFormulaName = 'if' | 'equal';
+export type TFormulaType = 'operator' | 'property' | 'function' | 'symbol' | 'constant';
+export type TFormulaResultType = 'number' | 'checkbox' | 'text' | 'date';
+export type TFormulaValueType = 'number' | 'string';
+export type TFormulaSymbolName = 'false' | 'true';
+
 export interface IFormulaArgsPropertyType {
-  type: "property",
-  id: string,
-  result_type: IFormulaResultType,
-  name: string
+	type: 'property';
+	id: string;
+	result_type: TFormulaResultType;
+	name: string;
 }
 
 export interface IFormulaArgsConstantType {
-  type: "constant",
-  result_type: IFormulaResultType,
-  value: string
-  value_type: IFormulaValueType
+	type: 'constant';
+	result_type: TFormulaResultType;
+	value: string;
+	value_type: TFormulaValueType;
 }
 
 export type TFormulaArgs = IFormulaArgsPropertyType | IFormulaArgsConstantType;
 
 export interface IFormulaArgsPropertyType {
-  type: "property",
-  id: string,
+	type: 'property';
+	id: string;
 }
 
-export type IFormulaName = "if" | "equal";
-export type IFormulaResultType = "number" | "checkbox" | "text";
-export type IFormulaType = "function" | "property" | "constant";
-export type IFormulaValueType = "number" | "string";
-
-export interface IFormula {
-  args: TFormulaArgs[],
-  name: IFormulaName,
-  result_type: IFormulaResultType,
-  type: IFormulaType
+export interface ISymbolCheckboxFormula {
+	name: TFormulaSymbolName;
+	result_type: 'checkbox';
+	type: 'symbol';
 }
+
+export type TCheckboxFormula = ISymbolCheckboxFormula | IPropertyFormula<'checkbox'>;
+
+export type TOperatorFormula = IEqualOperatorFormula;
+
+export interface IEqualOperatorFormula {
+	name: 'equal';
+	type: 'operator';
+	operator: '==';
+	result_type: 'checkbox';
+	args: [TCheckboxFormula, TCheckboxFormula];
+}
+
+export interface IPropertyFormula<RT extends TFormulaResultType> {
+	type: 'property';
+	id: string;
+	name: string;
+	result_type: RT;
+}
+
+export type TPropertyFormula =
+	| IPropertyFormula<'checkbox'>
+	| IPropertyFormula<'text'>
+	| IPropertyFormula<'date'>
+	| IPropertyFormula<'number'>;
+
+export type TFormula = TOperatorFormula | TPropertyFormula;
--- packages/types/src/schema.ts
+++ packages/types/src/schema.ts
@@ -1,112 +1,153 @@
-import { TTextColor } from ".";
+import { TTextColor, TFormula } from '.';
 
-export type TBasicSchemaUnitType = 'text' | 'number' | 'select' | 'multi_select' | 'title' | 'date' | 'person' | 'file' | 'checkbox' | 'url' | 'email' | 'phone_number';
-export type TAdvancedSchemaUnitType = 'formula' | 'relation' | 'rollup' | 'created_time' | 'created_by' | 'last_edited_time' | 'last_edited_by';
+export type TBasicSchemaUnitType =
+	| 'text'
+	| 'number'
+	| 'select'
+	| 'multi_select'
+	| 'title'
+	| 'date'
+	| 'person'
+	| 'file'
+	| 'checkbox'
+	| 'url'
+	| 'email'
+	| 'phone_number';
+
+export type TAdvancedSchemaUnitType =
+	| 'formula'
+	| 'relation'
+	| 'rollup'
+	| 'created_time'
+	| 'created_by'
+	| 'last_edited_time'
+	| 'last_edited_by';
+
 export type TSchemaUnitType = TBasicSchemaUnitType | TAdvancedSchemaUnitType;
 
 // ? TD:1:H Multiple ISchemaUnit interfaces for all types of schemaunit
 export interface ISchemaUnit {
-  name: string,
-  type: TSchemaUnitType,
+	name: string;
+	type: TSchemaUnitType;
 }
 
 export interface SelectOption {
-  id: string,
-  value: string,
-  color: TTextColor
+	id: string;
+	value: string;
+	color: TTextColor;
 }
 
 /* Basic Schema Units */
 
 export interface TextSchemaUnit extends ISchemaUnit {
-  type: "text",
+	type: 'text';
 }
 
 export interface NumberSchemaUnit extends ISchemaUnit {
-  type: "number"
+	type: 'number';
 }
 
 export interface SelectSchemaUnit extends ISchemaUnit {
-  type: "select",
-  options: SelectOption[]
+	type: 'select';
+	options: SelectOption[];
 }
 
 export interface MultiSelectSchemaUnit extends ISchemaUnit {
-  type: "multi_select",
-  options: SelectOption[]
+	type: 'multi_select';
+	options: SelectOption[];
 }
 
 export interface TitleSchemaUnit extends ISchemaUnit {
-  type: "title"
+	type: 'title';
 }
 
 export interface DateSchemaUnit extends ISchemaUnit {
-  type: "date"
+	type: 'date';
 }
 
 export interface PersonSchemaUnit extends ISchemaUnit {
-  type: "person"
+	type: 'person';
 }
 
 export interface FileSchemaUnit extends ISchemaUnit {
-  type: "file"
+	type: 'file';
 }
 
 export interface CheckboxSchemaUnit extends ISchemaUnit {
-  type: "checkbox"
+	type: 'checkbox';
 }
 
 export interface UrlSchemaUnit extends ISchemaUnit {
-  type: "url"
+	type: 'url';
 }
 
 export interface EmailSchemaUnit extends ISchemaUnit {
-  type: "email"
+	type: 'email';
 }
 
 export interface PhoneNumberSchemaUnit extends ISchemaUnit {
-  type: "phone_number"
+	type: 'phone_number';
 }
 
 export interface FormulaSchemaUnit extends ISchemaUnit {
-  type: "formula",
+	type: 'formula';
+	formula: TFormula;
 }
+
 export interface RelationSchemaUnit extends ISchemaUnit {
-  type: "relation",
-  collection_id: string,
-  property: string,
+	type: 'relation';
+	collection_id: string;
+	property: string;
 }
 
 export interface RollupSchemaUnit extends ISchemaUnit {
-  type: "rollup",
-  collection_id: string,
-  property: string,
-  relation_property: string,
-  target_property: string,
-  target_property_type: TSchemaUnitType,
+	type: 'rollup';
+	collection_id: string;
+	property: string;
+	relation_property: string;
+	target_property: string;
+	target_property_type: TSchemaUnitType;
 }
 
 export interface CreatedTimeSchemaUnit extends ISchemaUnit {
-  type: "created_time"
+	type: 'created_time';
 }
 export interface CreatedBySchemaUnit extends ISchemaUnit {
-  type: "created_by"
+	type: 'created_by';
 }
 export interface LastEditedTimeSchemaUnit extends ISchemaUnit {
-  type: "last_edited_time"
+	type: 'last_edited_time';
 }
 
 export interface LastEditedBySchemaUnit extends ISchemaUnit {
-  type: "last_edited_by"
+	type: 'last_edited_by';
 }
 
-export type TBasicSchemaUnit = TextSchemaUnit | NumberSchemaUnit | SelectSchemaUnit | MultiSelectSchemaUnit | TitleSchemaUnit | DateSchemaUnit | PersonSchemaUnit | FileSchemaUnit | CheckboxSchemaUnit | UrlSchemaUnit | EmailSchemaUnit | PhoneNumberSchemaUnit;
+export type TBasicSchemaUnit =
+	| TextSchemaUnit
+	| NumberSchemaUnit
+	| SelectSchemaUnit
+	| MultiSelectSchemaUnit
+	| TitleSchemaUnit
+	| DateSchemaUnit
+	| PersonSchemaUnit
+	| FileSchemaUnit
+	| CheckboxSchemaUnit
+	| UrlSchemaUnit
+	| EmailSchemaUnit
+	| PhoneNumberSchemaUnit;
 
-export type TAdvancedSchemaUnit = FormulaSchemaUnit | RelationSchemaUnit | RollupSchemaUnit | CreatedTimeSchemaUnit | CreatedBySchemaUnit | LastEditedTimeSchemaUnit | LastEditedBySchemaUnit;
+export type TAdvancedSchemaUnit =
+	| FormulaSchemaUnit
+	| RelationSchemaUnit
+	| RollupSchemaUnit
+	| CreatedTimeSchemaUnit
+	| CreatedBySchemaUnit
+	| LastEditedTimeSchemaUnit
+	| LastEditedBySchemaUnit;
 
 export type TSchemaUnit = TBasicSchemaUnit | TAdvancedSchemaUnit;
 
 export interface Schema {
-  [key: string]: TSchemaUnit
-};
-
+	[key: string]: TSchemaUnit;
+}

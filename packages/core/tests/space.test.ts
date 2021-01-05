eafc3d3e432b92b3f10a3fac4097010fd93fc082
Safwan Shaheer <devorein00@gmail.com>
2021-01-05 12:13:01 +0600

    TEST: Added tests for space.deleteTPages method

--- packages/core/tests/space.test.ts
+++ packages/core/tests/space.test.ts
@@ -133,7 +133,7 @@ function testDeleteMethod<P extends TData>({child_path, child_id, child_type = "
   }
 }
 
-describe("Getter methods for space", ()=>{
+describe.skip("Getter methods for space", ()=>{
   it("Get space_view",()=>{
     const space_view = space.getSpaceView();
     expect(space_view).not.toBeNull();
@@ -268,7 +268,7 @@ describe("Getter methods for space", ()=>{
   })
 })
 
-describe("Update methods for space", ()=>{
+describe.skip("Update methods for space", ()=>{
   beforeEach(()=>{
     space.clearStackSyncRecords();
   })
@@ -464,5 +464,35 @@ describe("Delete methods for space", ()=>{
       parent_type: "space",
       child_path: "pages"
     });  
+  })
+
+  it("Delete [root_page] cb", async ()=>{
+    await space.deleteTRootPages((page)=>page.type === "page" && page.id === ROOT_PAGE_ONE_ID)
+    testDeleteMethod<ISpace>({
+      child_id: ROOT_PAGE_ONE_ID,
+      parent_id: SPACE_ONE_ID,
+      parent_type: "space",
+      child_path: "pages"
+    })
+  })
+
+  it("Delete [root_cvp] [id]", async()=>{
+    await space.deleteTRootPages([ROOT_COLLECTION_VIEW_PAGE_ONE_ID]);
+    testDeleteMethod<ISpace>({
+      child_id: ROOT_COLLECTION_VIEW_PAGE_ONE_ID,
+      parent_id: SPACE_ONE_ID,
+      parent_type: "space",
+      child_path: "pages"
+    })
+  })
+
+  it("Delete [root_cvp] cb", async()=>{
+    await space.deleteTRootPages(page=>page.type === "collection_view_page" && page.id === ROOT_COLLECTION_VIEW_PAGE_ONE_ID);
+    testDeleteMethod<ISpace>({
+      child_id: ROOT_COLLECTION_VIEW_PAGE_ONE_ID,
+      parent_id: SPACE_ONE_ID,
+      parent_type: "space",
+      child_path: "pages"
+    })
   })
 })

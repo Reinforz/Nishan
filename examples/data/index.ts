cedf1730eef772b726764545d6c1545243f4da4a
Safwan Shaheer <devorein00@gmail.com>
2021-01-09 15:41:40 +0600

    FEAT(wf): Added workflow_2 boilerplates and purpose file

--- examples/data/index.ts
+++ examples/data/index.ts
@@ -1,15 +1,10 @@
-import phase from "./phase"
-import priority from "./priority"
-import status from "./status"
-import difficulty from "./difficulty"
-import subject from "./subject"
-export * from "./category"
-export * from "./for"
+import phase from './phase';
+import priority from './priority';
+import status from './status';
+import difficulty from './difficulty';
+import subject from './subject';
+import purpose from './purpose';
+export * from './category';
+export * from './for';
 
-export {
-  phase,
-  priority,
-  status,
-  subject,
-  difficulty
-}
+export { phase, priority, status, subject, difficulty, purpose };
---
+++ examples/data/purpose.ts
@@ -0,0 +1,32 @@
+import { SelectOption } from '../../packages/core/dist/Nishan';
+
+export default [
+	{
+		color: 'default',
+		value: 'Learning'
+	},
+	{
+		color: 'default',
+		value: 'Practice'
+	},
+	{
+		color: 'default',
+		value: 'Project'
+	},
+	{
+		color: 'default',
+		value: 'Revise'
+	},
+	{
+		color: 'default',
+		value: 'Organization'
+	},
+	{
+		color: 'default',
+		value: 'Resources'
+	},
+	{
+		color: 'default',
+		value: 'Online'
+	}
+] as (Omit<SelectOption, 'id'>)[];

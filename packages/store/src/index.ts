a581eba37398ca6c44b737600bed1d80ed27d9a5
Safwan Shaheer <devorein00@gmail.com>
2021-01-15 13:54:13 +0600

    FEAT(store): Added storing collection and views data of collection_block

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { SyncRecordValuesResult } from '@nishans/types';
+import { ICollection, SyncRecordValuesResult, TCollectionBlock, TView } from '@nishans/types';
 import fs from 'fs';
 
 const idToUuid = (id = '') => {
@@ -13,12 +13,13 @@ export async function storeLocally (token: string, database_id: string, path: st
 			cookie: `token_v2=${token};`
 		}
 	};
+	database_id = idToUuid(database_id);
 	const { data } = await axios.post<SyncRecordValuesResult>(
 		'https://www.notion.so/api/v3/syncRecordValues',
 		{
 			requests: [
 				{
-					id: idToUuid(database_id),
+					id: database_id,
 					table: 'block',
 					version: 0
 				}
@@ -27,19 +28,32 @@ export async function storeLocally (token: string, database_id: string, path: st
 		headers
 	);
 
-	await fs.promises.writeFile(path, JSON.stringify(data, null, 2), 'utf-8');
-	/* try {
-		const { data } = await axios.post<GetSpacesResult>(
-			'https://www.notion.so/api/v3/getSpaces',
-			{},
-			{
-				headers: {
-					cookie: `token_v2=${token}`
-				}
-			}
-		);
-		console.log(data);
-	} catch (err) {
-		throw new Error(err.response.data);
-	} */
+	const collection_block_data = data.recordMap.block[database_id].value as TCollectionBlock;
+	const { collection_id, view_ids } = collection_block_data;
+
+	const { data: { recordMap } } = await axios.post<SyncRecordValuesResult>(
+		'https://www.notion.so/api/v3/syncRecordValues',
+		{
+			requests: [
+				{
+					id: collection_id,
+					table: 'collection',
+					version: 0
+				},
+				...view_ids.map((view_id) => ({ id: view_id, table: 'collection_view', version: 0 }))
+			]
+		},
+		headers
+	);
+
+	const collection_data = recordMap.collection[collection_id].value as ICollection;
+	const views_data = Object.values(recordMap.collection_view).map(({ value }) => value) as TView[];
+
+	const result_data = {
+		collection_block: collection_block_data,
+		collection: collection_data,
+		views: views_data
+	};
+
+	await fs.promises.writeFile(path, JSON.stringify(result_data, null, 2), 'utf-8');
 }

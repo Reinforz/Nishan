6b84db352b6f9ef42be4a7cf14e2d220b7dd53d0
Safwan Shaheer <devorein00@gmail.com>
2021-01-16 23:30:23 +0600

    FEAT(filters): Removed unncessary redux code from filters

--- packages/filters/src/features/counter/Counter.tsx
+++ packages/filters/src/features/counter/Counter.tsx
@@ -4,10 +4,8 @@ import {
   decrement,
   increment,
   incrementByAmount,
-  incrementAsync,
   selectCount,
 } from './counterSlice';
-import styles from './Counter.module.css';
 
 export function Counter() {
   const count = useSelector(selectCount);
@@ -16,43 +14,33 @@ export function Counter() {
 
   return (
     <div>
-      <div className={styles.row}>
+      <div>
         <button
-          className={styles.button}
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           +
         </button>
-        <span className={styles.value}>{count}</span>
+        <span>{count}</span>
         <button
-          className={styles.button}
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
       </div>
-      <div className={styles.row}>
+      <div>
         <input
-          className={styles.textbox}
           aria-label="Set increment amount"
           value={incrementAmount}
           onChange={e => setIncrementAmount(e.target.value)}
         />
         <button
-          className={styles.button}
           onClick={() =>
             dispatch(incrementByAmount(Number(incrementAmount) || 0))
           }
         >
           Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(Number(incrementAmount) || 0))}
-        >
-          Add Async
         </button>
       </div>
     </div>

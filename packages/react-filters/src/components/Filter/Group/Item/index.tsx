2156342b3768855dece572671c0db8cc23edb4b6
Safwan Shaheer <devorein00@gmail.com>
2021-01-18 23:59:39 +0600

    IMP(react-filters): First group item shows where, 2nd group operator component and the rest group operator

--- packages/react-filters/src/components/Filter/Group/Item/index.tsx
+++ packages/react-filters/src/components/Filter/Group/Item/index.tsx
@@ -7,6 +7,7 @@ import FilterGroupItemOperator from "./Operator";
 import FilterGroupItemOptions from "./Options";
 import FilterGroupItemProperty from "./Property";
 import FilterGroupItemValue from "./Value";
+import FilterGroupOperator from "../Operator"
 
 interface Props {
   parent_filter: IViewFilter,
@@ -16,12 +17,13 @@ interface Props {
 
 export default function FilterGroupItem({ parent_filter, filter, trails }: Props) {
   const { schema } = useContext(NotionFilterContext)
-
+  const last_trail = trails[trails.length - 1];
   if ((filter as IViewFilter).operator) return <FilterGroup parent_filter={parent_filter} filter={filter as IViewFilter} trails={trails} />
   const schema_unit = schema[(filter as TViewFilters).property];
   const filter_info = getFilterInfo(schema_unit);
 
   return <div className="NotionFilter-Group-Item" style={{ display: "flex", border: "2px solid black" }}>
+    {last_trail === 0 ? null : last_trail === 1 ? <FilterGroupOperator filter={parent_filter} /> : parent_filter.operator}
     <FilterGroupItemProperty filter={filter as TViewFilters} />
     <FilterGroupItemOperator operators={filter_info} filter={filter as TViewFilters} />
     <FilterGroupItemValue value="string" />
--- packages/react-filters/src/components/Filter/Group/index.tsx
+++ packages/react-filters/src/components/Filter/Group/index.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import { FilterGroupProps } from "../../../types";
 import FilterGroupAdd from "./Add";
 import FilterGroupItem from "./Item";
-import FilterGroupOperator from "./Operator";
 import FilterGroupOptions from "./Options";
 
 export default function FilterGroup(props: FilterGroupProps) {
   const { filter, trails } = props;
   return <div className="NotionFilter-Group">
     {filter.filters.length !== 0 ? <div style={{ display: "flex", border: "1px solid black", padding: "10px" }}>
-      <FilterGroupOperator filter={filter} />
+      <div>Where</div>
       <div className="NotionFilter-Group-Items">
         {filter.filters.map((child_filter, index) => <FilterGroupItem parent_filter={filter} key={index} filter={child_filter} trails={trails.concat(index)} />)}
       </div>

21f8bf802cabf477b9d51c9c151ee721ac35c428
Safwan Shaheer <devorein00@gmail.com>
2021-01-18 13:58:59 +0600

    FEAT(react-filters): Added filterItemProperty handler

--- packages/react-filters/src/components/Filter/Group/Item/Property/index.tsx
+++ packages/react-filters/src/components/Filter/Group/Item/Property/index.tsx
@@ -1,10 +1,18 @@
+import { TViewFilters } from "@nishans/types";
 import React, { useContext } from "react";
 import { NotionFilterContext } from "../../../../../NotionFilter";
 import { BasicSelect } from "../../../../Shared";
 
-export default function FilterGroupItemProperty() {
-  const { schema } = useContext(NotionFilterContext)
+interface Props {
+  filter: TViewFilters
+}
+
+export default function FilterGroupItemProperty({ filter }: Props) {
+  const { schema, filters, setFilters } = useContext(NotionFilterContext)
   return <div className="NotionFilter-Group-Item-Property">
-    <BasicSelect label="Property" value={'and'} onChange={() => { }} items={Object.entries(schema).map(([key, value]) => ({ label: value.name, value: key }))} />
+    <BasicSelect label="Property" value={filter.property} onChange={(e) => {
+      filter.property = e.target.value as string;
+      setFilters({ ...filters })
+    }} items={Object.entries(schema).map(([key, value]) => ({ label: value.name, value: key }))} />
   </div>
 }
--- packages/react-filters/src/components/Filter/Group/Item/index.tsx
+++ packages/react-filters/src/components/Filter/Group/Item/index.tsx
@@ -1,6 +1,7 @@
 import { IViewFilter, TViewFilters } from "@nishans/types";
 import React from "react";
 import FilterGroup from "..";
+import { getFilterInfo } from "../../../../utils/getFilterInfoFromSchemaUnit";
 import FilterGroupItemOperator from "./Operator";
 import FilterGroupItemOptions from "./Options";
 import FilterGroupItemProperty from "./Property";
@@ -15,7 +16,7 @@ interface Props {
 export default function FilterGroupItem({ parent_filter, filter, trails }: Props) {
   if ((filter as IViewFilter).operator) return <FilterGroup filter={filter as IViewFilter} trails={trails} />
   return <div className="NotionFilter-Group-Item" style={{ display: "flex", border: "2px solid black" }}>
-    <FilterGroupItemProperty />
+    <FilterGroupItemProperty filter={filter as TViewFilters} />
     <FilterGroupItemOperator operators={[{ value: "checkbox_is", label: "Is" }]} />
     <FilterGroupItemValue value="string" />
     <FilterGroupItemOptions parent_filter={parent_filter} trails={trails} />

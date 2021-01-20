5bbd81fcbc9ad294c7635d9a611df4c49ff79f2c
Safwan Shaheer <devorein00@gmail.com>
2021-01-20 18:06:39 +0600

    FEAT(md): Added uploadMarkdownFiles to upload multiple markdown files

--- packages/markdown/src/uploadMarkdown.ts
+++ packages/markdown/src/uploadMarkdown.ts
@@ -1,4 +1,4 @@
-import { ISpace } from '@nishans/types';
+import { IOperation, ISpace } from '@nishans/types';
 import {
 	uploadToNotion,
 	initializeNotion,
@@ -13,10 +13,28 @@ interface UploadMarkdownFileParams {
 	getSpace?: (space: ISpace) => any;
 }
 
+interface UploadMarkdownFilesParams {
+	filepaths: string[];
+	token: UploadMarkdownFileParams['token'];
+	getSpace?: UploadMarkdownFileParams['getSpace'];
+}
+
 export async function uploadMarkdownFile ({ getSpace, filepath, token }: UploadMarkdownFileParams) {
 	const tree = await parseFile(filepath);
 	const notion_data = await initializeNotion(token, getSpace);
 	const { blocks, config } = await mdast2NotionBlocks(tree);
 	const notion_block_ops = await generateNotionBlockOperations(notion_data, blocks, config);
 	await uploadToNotion(notion_data, notion_block_ops);
+}
+
+export async function uploadMarkdownFiles ({ getSpace, filepaths, token }: UploadMarkdownFilesParams) {
+	const operations: IOperation[] = [];
+	const notion_data = await initializeNotion(token, getSpace);
+	for (let index = 0; index < filepaths.length; index++) {
+		const filepath = filepaths[index];
+		const tree = await parseFile(filepath);
+		const { blocks, config } = await mdast2NotionBlocks(tree);
+		operations.push(...(await generateNotionBlockOperations(notion_data, blocks, config)));
+	}
+	await uploadToNotion(notion_data, operations);
 }

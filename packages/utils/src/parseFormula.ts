5cfcce7de5dd0a9484140d7b51a82009e8b787e6
Safwan Shaheer <devorein00@gmail.com>
2021-01-21 00:20:30 +0600

    FEAT(utils): Added property reference support for parseFormula

--- packages/utils/src/parseFormula.ts
+++ packages/utils/src/parseFormula.ts
@@ -1,11 +1,18 @@
 const FUNCTION_REGEX = /^(concat|join|slice|toNumber|contains|replace|replaceAll|test|empty|abs|cbrt|ceil|exp|floor|ln|log10|log2|min|max|round|sign|sqrt|start|end|now|timestamp|fromTimestamp|dateAdd|dateSubtract|dateBetween|formatDate|minute|hour|day|date|month|year|unaryMinus|unaryPlus|add|subtract|multiply|divide|pow|mod|and|or|larger|largerEq|smaller|smallerEq|not|length|format|equal|unequal|if)\((.+)\)/;
-const ARGS_REGEX = /((?:concat|join|slice|toNumber|contains|replace|replaceAll|test|empty|abs|cbrt|ceil|exp|floor|ln|log10|log2|min|max|round|sign|sqrt|start|end|now|timestamp|fromTimestamp|dateAdd|dateSubtract|dateBetween|formatDate|minute|hour|day|date|month|year|unaryMinus|unaryPlus|add|subtract|multiply|divide|pow|mod|and|or|larger|largerEq|smaller|smallerEq|not|length|format|equal|unequal|if)\(.+\))|(\w+)|(e|pi|true|false)|(\d+)/g;
+const ARGS_REGEX = /((?:concat|join|slice|toNumber|contains|replace|replaceAll|test|empty|abs|cbrt|ceil|exp|floor|ln|log10|log2|min|max|round|sign|sqrt|start|end|now|timestamp|fromTimestamp|dateAdd|dateSubtract|dateBetween|formatDate|minute|hour|day|date|month|year|unaryMinus|unaryPlus|add|subtract|multiply|divide|pow|mod|and|or|larger|largerEq|smaller|smallerEq|not|length|format|equal|unequal|if)\(([^()]+)\))|"(\w+)"|(e|pi|true|false)|(\d+)|prop\("(?:(\w*\s*\d*)+)"\)/g;
 
 function parseArg (arg: string) {
 	const function_match = arg.match(FUNCTION_REGEX);
 	if (function_match) {
 		return parseFormula(function_match[0]);
-	} else if (arg.match(/(true|false)/)) {
+  } else if(arg.match(/^(e|pi)$/)){
+    return {
+			type: 'symbol',
+			name: arg,
+			result_type: 'number'
+		};
+  }
+  else if (arg.match(/(true|false)/)) {
 		return {
 			type: 'symbol',
 			name: arg,
@@ -18,10 +25,17 @@ function parseArg (arg: string) {
 			value_type: 'number',
 			result_type: 'number'
 		};
-	} else
+  } else if(arg.match(/prop\("(?:(\w*\s*\d*)+)"\)/)){
+    const property_name = arg.match(/prop\("(.+)"\)/)
+    return {
+      type: "property",
+      name: property_name?.[1]
+    }
+  }
+  else
 		return {
 			type: 'constant',
-			value: arg,
+			value: arg.replace(/("|")/g, ''),
 			value_type: 'string',
 			result_type: 'text'
 		};
@@ -38,7 +52,6 @@ export function parseFormula (formula: string) {
 		const [ , function_name, args ] = match;
 		result_formula.function = function_name;
 		const args_match = args.match(ARGS_REGEX);
-
 		if (args_match) args_match.forEach((arg) => result_formula.args.push(parseArg(arg)));
 	}
 	return result_formula;

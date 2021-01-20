dae92f66bad97b24ce429c724a86802ce12c7329
Safwan Shaheer <devorein00@gmail.com>
2021-01-20 18:11:29 +0600

    RF(md): Refactored uploadmd functions to utilize util function

--- packages/markdown/src/uploadMarkdown.ts
+++ packages/markdown/src/uploadMarkdown.ts
@@ -6,6 +6,7 @@ import {
 	parseFile,
 	generateNotionBlockOperations
 } from '../utils';
+import { NotionOperationData } from './types';
 
 interface UploadMarkdownFileParams {
 	filepath: string;
@@ -19,22 +20,23 @@ interface UploadMarkdownFilesParams {
 	getSpace?: UploadMarkdownFileParams['getSpace'];
 }
 
-export async function uploadMarkdownFile ({ getSpace, filepath, token }: UploadMarkdownFileParams) {
+async function generateNotionBlockOperationsFromMarkdown (filepath: string, notion_data: NotionOperationData) {
 	const tree = await parseFile(filepath);
-	const notion_data = await initializeNotion(token, getSpace);
 	const { blocks, config } = await mdast2NotionBlocks(tree);
-	const notion_block_ops = await generateNotionBlockOperations(notion_data, blocks, config);
+	return await generateNotionBlockOperations(notion_data, blocks, config);
+}
+
+export async function uploadMarkdownFile ({ token, getSpace, filepath }: UploadMarkdownFileParams) {
+	const notion_data = await initializeNotion(token, getSpace);
+	const notion_block_ops = await generateNotionBlockOperationsFromMarkdown(filepath, notion_data);
 	await uploadToNotion(notion_data, notion_block_ops);
 }
 
 export async function uploadMarkdownFiles ({ getSpace, filepaths, token }: UploadMarkdownFilesParams) {
 	const operations: IOperation[] = [];
 	const notion_data = await initializeNotion(token, getSpace);
-	for (let index = 0; index < filepaths.length; index++) {
-		const filepath = filepaths[index];
-		const tree = await parseFile(filepath);
-		const { blocks, config } = await mdast2NotionBlocks(tree);
-		operations.push(...(await generateNotionBlockOperations(notion_data, blocks, config)));
-	}
+	for (let index = 0; index < filepaths.length; index++)
+		operations.push(...(await generateNotionBlockOperationsFromMarkdown(filepaths[index], notion_data)));
+
 	await uploadToNotion(notion_data, operations);
 }

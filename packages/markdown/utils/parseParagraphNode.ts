14636c60388b55749aa94c3cb5a0a29358609710
Safwan Shaheer <devorein00@gmail.com>
2021-01-21 12:49:45 +0600

    RF(md): Refactored parseParagraphNode to utilize formatText util

--- packages/markdown/utils/parseParagraphNode.ts
+++ packages/markdown/utils/parseParagraphNode.ts
@@ -1,31 +1,44 @@
 import { Node } from 'unist';
 import { inlineText, chunk } from './inlineBlock';
 
+function formatText (formats: ('b' | 'i' | '_' | 's')[], block: chunk) {
+	formats.forEach((format) => {
+		switch (format) {
+			case 's':
+				block.strikeThrough;
+				break;
+			case '_':
+				block.underline;
+				break;
+			case 'i':
+				block.italic;
+				break;
+			case 'b':
+				block.bold;
+				break;
+		}
+	});
+}
+
 export function parseParagraphNode (paragraph: Node): string[][] {
 	const block = inlineText('');
 
-	function inner (block: chunk, parent: Node, formats: ('bold' | 'italic')[]) {
+	function inner (block: chunk, parent: Node, formats: ('b' | 'i' | '_' | 's')[]) {
 		(parent as any).children.forEach((child: Node) => {
 			switch (child.type) {
 				case 'text':
 					block.add((child as any).value);
-					formats.forEach((format) => {
-						if (format === 'bold') block.bold;
-						else if (format === 'italic') block.italic;
-					});
+					formatText(formats, block);
 					break;
 				case 'emphasis':
-					inner(block, child as any, formats.concat('italic'));
+					inner(block, child as any, formats.concat('i'));
 					break;
 				case 'strong':
-					inner(block, child as any, formats.concat('bold'));
+					inner(block, child as any, formats.concat('b'));
 					break;
 				case 'inlineCode':
 					block.add((child as any).value).code;
-					formats.forEach((format) => {
-						if (format === 'bold') block.bold;
-						else if (format === 'italic') block.italic;
-					});
+					formatText(formats, block);
 					break;
 			}
 		});

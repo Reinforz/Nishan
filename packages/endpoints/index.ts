ccfd6c5f6e8c55471d984a3949e1eb9585d7dc40
Safwan Shaheer <devorein00@gmail.com>
2021-01-21 18:30:05 +0600

    DOC(endpoints): Added doc and relocated files

--- packages/endpoints/index.ts
+++
@@ -1,8 +0,0 @@
-import Queries from './src/Queries';
-import Mutations from './src/Mutations';
-import Cache from './src/Cache';
-
-export { Cache, Queries, Mutations };
-
-export * from './src/types';
-export * from './utils';
---
+++ packages/endpoints/src/index.ts
@@ -0,0 +1,8 @@
+import Queries from './Queries';
+import Mutations from './Mutations';
+import Cache from './Cache';
+
+export { Cache, Queries, Mutations };
+
+export * from './types';
+export * from '../utils';

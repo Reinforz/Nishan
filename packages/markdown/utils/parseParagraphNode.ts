d4ed3d32093130d13edf9c71a19faf07eb201ca2
Safwan Shaheer <devorein00@gmail.com>
2021-01-21 16:37:53 +0600

    RF(md): Refactored to use parseTextFormatters

--- packages/markdown/utils/parseParagraphNode.ts
+++ packages/markdown/utils/parseParagraphNode.ts
@@ -21,25 +21,29 @@ function formatText (formats: ('b' | 'i' | '_' | 's')[], block: chunk) {
 	});
 }
 
+export function parseTextFormatters(text: string, formats: ('b' | 'i' | '_' | 's')[], block: chunk){
+  const matches = (text as string).match(/(\[(?:\w+=\w+)\]{(?:\w?\s?)+})|(\w?\s?)+/g);
+  matches?.forEach(match=>{
+    const contains_format = match.match(/^\[(\w+=\w+)\]{((?:\w?\s?)+)}/);
+    if(contains_format) {
+      const [, format, text] = contains_format, [highlight, color] = format.split("=");
+      block.add(text);
+      block.highlight((highlight === "c" ? color : color+"_background") as TFormatBlockColor)
+      formatText(formats, block)
+    }else{
+      block.add(match);
+      formatText(formats, block);
+    }
+  })
+}
+
 export function parseParagraphNode (paragraph: Node): string[][] {
 	const block = inlineText();
 	function inner (block: chunk, parent: Node, formats: ('b' | 'i' | '_' | 's')[]) {
 		(parent as any).children.forEach((child: Node) => {
 			switch (child.type) {
 				case 'text':
-          const {value: text} = child, matches = (text as string).match(/(\[(?:\w+=\w+)\]{(?:\w?\s?)+})|(\w?\s?)+/g);
-          matches?.forEach(match=>{
-            const contains_format = match.match(/^\[(\w+=\w+)\]{((?:\w?\s?)+)}/);
-            if(contains_format) {
-              const [, format, text] = contains_format, [highlight, color] = format.split("=");
-              block.add(text);
-              block.highlight((highlight === "c" ? color : color+"_background") as TFormatBlockColor)
-              formatText(formats, block)
-            }else{
-              block.add(match);
-              formatText(formats, block);
-            }
-          })
+          parseTextFormatters(child.value as string, formats, block)
 					break;
 				case 'emphasis':
 					inner(block, child as any, formats.concat('i'));
@@ -48,8 +52,7 @@ export function parseParagraphNode (paragraph: Node): string[][] {
 					inner(block, child as any, formats.concat('b'));
 					break;
 				case 'inlineCode':
-					block.add((child as any).value).code;
-					formatText(formats, block);
+          parseTextFormatters(child.value as string, formats, block)
 					break;
 			}
 		});

083d984970526226af53297bd84efb410437cd45
Safwan Shaheer <devorein00@gmail.com>
2021-01-26 16:35:22 +0600

    FEAT(remark-notion): Converged underline and callout plugin into one

--- packages/remark-notion/src/index.ts
+++ packages/remark-notion/src/index.ts
@@ -1,7 +1,56 @@
 const regex = /^\|>\s?\[(?<config>(?:.+?=.+?,?)+)\]$/m;
 
+function whitespace (character: string | number) {
+	return /\s/.test(typeof character === 'number' ? String.fromCharCode(character) : character.charAt(0));
+}
+
 export default function plugin () {
-	function defaultTokenizer (eat, value: string) {
+  const UNDERLINE_CHAR = '-',
+		DOUBLE_UNDERLINE_CHAR = UNDERLINE_CHAR + UNDERLINE_CHAR;
+
+  function locator (value: string, fromIndex: number) {
+    return value.indexOf(DOUBLE_UNDERLINE_CHAR, fromIndex);
+  }
+
+  function underlineTokenizer (eat, value: string) {
+		if (
+			// Checks to see if value starts with
+			!value.startsWith(DOUBLE_UNDERLINE_CHAR) ||
+			value.startsWith(DOUBLE_UNDERLINE_CHAR + DOUBLE_UNDERLINE_CHAR) ||
+			whitespace(value.charAt(2))
+		) {
+			return;
+		}
+
+    let current = '', 
+      previous = '', 
+      preceding = '', 
+      subvalue = '', 
+      index = 1;
+		const length = value.length;
+		const now = eat.now();
+		now.column += 2;
+		now.offset += 2;
+
+		while (++index < length) {
+			current = value.charAt(index);
+
+			if (current === UNDERLINE_CHAR && previous === UNDERLINE_CHAR && (!preceding || !whitespace(preceding))) {
+				return eat(DOUBLE_UNDERLINE_CHAR + subvalue + DOUBLE_UNDERLINE_CHAR)({
+					type: 'underline',
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					children: this.tokenizeInline(subvalue, now)
+				});
+			}
+
+			subvalue += previous;
+			preceding = previous;
+			previous = current;
+		}
+	}
+
+	function calloutTokenizer (eat, value: string) {
 		if (value.trim().startsWith('|>')) {
 			const m = regex.exec(value.trim());
 			if (m) {
@@ -39,11 +88,13 @@ export default function plugin () {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
 	const Parser = this.Parser;
-	const blockTokenizers = Parser.prototype.blockTokenizers;
-	const blockMethods = Parser.prototype.blockMethods;
+	const {blockTokenizers, inlineTokenizers} = Parser.prototype;
+	const {blockMethods, inlineMethods} = Parser.prototype;
 
-	const insertPoint = blockMethods.indexOf('fencedCode') + 1;
-
-	blockTokenizers.container = defaultTokenizer;
-	blockMethods.splice(insertPoint, 0, 'container');
+	blockTokenizers.container = calloutTokenizer;
+  blockMethods.splice(blockMethods.indexOf('fencedCode'), 0, 'container');
+  
+  inlineTokenizers.underline = underlineTokenizer;
+	underlineTokenizer.locator = locator;
+  inlineMethods.splice(inlineMethods.indexOf('text'), 0, 'underline');
 }

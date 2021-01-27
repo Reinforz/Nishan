890432d25f63bcf06dc6a1368c4dda820710e3b5
Safwan Shaheer <devorein00@gmail.com>
2021-01-27 23:11:40 +0600

    RF(nf): Added generateFormulaArgFromProperty util function

--- packages/notion-formula/src/generateASTFromObject.ts
+++ packages/notion-formula/src/generateASTFromObject.ts
@@ -1,5 +1,5 @@
 import { TFormula, TFormulaResultType } from "@nishans/types";
-import { formulateResultTypeFromSchemaType, formula_rt_map, generateFormulaArgs } from "../utils";
+import { generateFormulaArgFromProperty, formula_rt_map, generateFormulaArgs } from "../utils";
 import { FormulaSchemaUnitInput, ISchemaMap, TResultType, TFormulaCreateInput } from "../types";
 
 export function parseFormulaFromObject (formula: FormulaSchemaUnitInput['formula'], schema_map: ISchemaMap): TFormula {
@@ -17,26 +17,12 @@ export function parseFormulaFromObject (formula: FormulaSchemaUnitInput['formula
 				result_type: result_type ?? (formula as any).result_type,
 				args: temp_args
       });
-      // ? FEAT:1:E unknown function used
 			if (Array.isArray(args))
 				for (let index = 0; index < args.length; index++) traverseFormula(temp_args, args[index] as any);
 			else traverseFormula(temp_args, args);
-		} else if((formula as { property: string }).property){
-			const schema_name = (formula as { property: string }).property,
-				result = schema_map.get(schema_name);
-			if (result) {
-				const { schema_id, type } = result;
-				let result_type: TFormulaResultType = '' as any;
-				if (result.type === 'formula') result_type = result.formula.result_type;
-				else result_type = formulateResultTypeFromSchemaType(type);
-				parent.push({
-					type: 'property',
-					id: schema_id,
-					name: result.name,
-					result_type
-				});
-			}
-    }else 
+		} else if((formula as { property: string }).property)
+      parent.push(generateFormulaArgFromProperty(formula as {property: string}, schema_map))
+    else 
       parent.push(generateFormulaArgs(formula))
 	}
 
--- packages/notion-formula/utils/generateFormulaArgs.ts
+++ packages/notion-formula/utils/generateFormulaArgs.ts
@@ -1,3 +1,7 @@
+import { TFormulaResultType } from '@nishans/types';
+import { ISchemaMap } from '../types/formula-object';
+import { formulateResultTypeFromSchemaType } from './returnFormulaResultType';
+
 export function generateFormulaArgs (value: any) {
 	if (typeof value === 'boolean')
 		return {
@@ -25,4 +29,21 @@ export function generateFormulaArgs (value: any) {
 			value_type: 'string',
 			result_type: 'text'
 		};
+}
+
+export function generateFormulaArgFromProperty (arg: { property: string }, schema_map: ISchemaMap) {
+	const schema_name = arg.property,
+		result = schema_map.get(schema_name);
+	if (result) {
+		const { schema_id, type } = result;
+		let result_type: TFormulaResultType = '' as any;
+		if (result.type === 'formula') result_type = result.formula.result_type;
+		else result_type = formulateResultTypeFromSchemaType(type);
+		return {
+			type: 'property',
+			id: schema_id,
+			name: result.name,
+			result_type
+		};
+	}
 }

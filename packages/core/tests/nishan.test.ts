c1abfa8fc1eb3f9bd35483c1353d0118de14da08
Safwan Shaheer <devorein00@gmail.com>
2021-01-27 17:33:12 +0600

    TEST+RF(core): Added and integrated util function cycleThroughInfoarr

--- packages/core/tests/nishan.test.ts
+++ packages/core/tests/nishan.test.ts
@@ -1,8 +1,8 @@
 import { INotionUser } from '@nishans/types';
 import { NotionUser } from '../src';
 import { TEST_DATA, nishan } from './constants';
+import { cycleThroughInfoarr } from './utils/cycleThroughInfoarr';
 import { generateTestInfo } from './utils/generateTestInfo';
-import { amount_arr, result_arr, way_arr } from './utils/testinfos';
 
 it('Sets up default configuration for Nishan', () => {
 	expect(nishan.interval).toBe(500);
@@ -13,13 +13,9 @@ const info = generateTestInfo<INotionUser, NotionUser, ['getNotionUser', 'getNot
 	TEST_DATA.notion_user
 );
 
-amount_arr.map((amount) => {
-	result_arr.map((result) => {
-		way_arr.map((way) => {
-			const msg = `Get ${amount} ${result} notion_user using ${result} ${way}`;
-			it(msg, async () => {
-				info[amount].checker(await (nishan as any)[info[amount].method](info[amount][result][way]), result);
-			});
-		});
+cycleThroughInfoarr((amount, result, way) => {
+	const msg = `Get ${amount} ${result} notion_user using ${result} ${way}`;
+	it(msg, async () => {
+		info[amount].checker(await (nishan as any)[info[amount].method](info[amount][result][way]), result);
 	});
 });

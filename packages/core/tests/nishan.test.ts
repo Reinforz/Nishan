53b1d95da657bc5e3c8b6bdb585b06a3759a8ac4
Safwan Shaheer <devorein00@gmail.com>
2021-01-27 17:05:37 +0600

    TEST+FEAT(core): Integrated generateTestInfo function into nishan.test

--- packages/core/tests/nishan.test.ts
+++ packages/core/tests/nishan.test.ts
@@ -1,40 +1,17 @@
 import { INotionUser } from '@nishans/types';
 import { NotionUser } from '../src';
 import { TEST_DATA, nishan } from './constants';
-import { TestInfo } from './types';
-import { checkMultiple, checkSingle } from './utils/checker';
+import { generateTestInfo } from './utils/generateTestInfo';
 import { amount_arr, result_arr, way_arr } from './utils/testinfos';
 
 it('Sets up default configuration for Nishan', () => {
 	expect(nishan.interval).toBe(500);
 });
 
-const info: TestInfo<INotionUser, NotionUser, ['getNotionUser', 'getNotionUsers']> = {
-	single: {
-		correct: {
-			id: TEST_DATA.notion_user[0].id.correct,
-			cb: (user) => user.id === TEST_DATA.notion_user[0].id.correct
-		},
-		incorrect: {
-			id: TEST_DATA.notion_user[0].id.incorrect,
-			cb: (user) => user.id === TEST_DATA.notion_user[0].id.incorrect
-		},
-		method: 'getNotionUser',
-		checker: checkSingle(TEST_DATA.notion_user[0])
-	},
-	multiple: {
-		correct: {
-			id: [ TEST_DATA.notion_user[0].id.correct ],
-			cb: (user) => user.id === TEST_DATA.notion_user[0].id.correct
-		},
-		incorrect: {
-			id: [ TEST_DATA.notion_user[0].id.incorrect ],
-			cb: (user) => user.id === TEST_DATA.notion_user[0].id.incorrect
-		},
-		method: 'getNotionUsers',
-		checker: checkMultiple(TEST_DATA.notion_user[0])
-	}
-};
+const info = generateTestInfo<INotionUser, NotionUser, ['getNotionUser', 'getNotionUsers']>(
+	[ 'getNotionUser', 'getNotionUsers' ],
+	TEST_DATA.notion_user
+);
 
 amount_arr.map((amount) => {
 	result_arr.map((result) => {

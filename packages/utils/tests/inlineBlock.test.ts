ed8654561d56ffc71eceec2f17955883d54495d7
Safwan Shaheer <devorein00@gmail.com>
2021-01-27 10:59:51 +0600

    TEST(utils): Added util method for testing stringified array

--- packages/utils/tests/inlineBlock.test.ts
+++ packages/utils/tests/inlineBlock.test.ts
@@ -1,42 +1,40 @@
+import { TTextFormat } from '@nishans/types';
 import { inlineText } from '../src';
 
-function createInlineTextFormatterSnapshot (formatter: string) {
-	return `
-  Array [
-    Array [
-      "text",
-      Array [
-        Array [
-          "${formatter}",
-        ],
-      ],
-    ],
-  ]
-`;
+function testAfterStringifying (source: TTextFormat, format: string[]) {
+	expect(JSON.stringify(source)).toBe(`[["text",[[${format.map((format) => `"${format}"`).join(',')}]]]]`);
 }
 
 describe('Should create correct formatting', () => {
 	it('Should support empty text', () => {
-		expect(inlineText().text).toMatchInlineSnapshot(`Array []`);
+		expect(JSON.stringify(inlineText().text)).toBe(`[]`);
 	});
 
 	it('Should support strikethrough formatting', () => {
-		expect(inlineText('text').strikeThrough.text).toMatchInlineSnapshot(createInlineTextFormatterSnapshot('s'));
+		testAfterStringifying(inlineText('text').strikeThrough.text, [ 's' ]);
 	});
 
 	it('Should support code formatting', () => {
-		expect(inlineText('text').code.text).toMatchInlineSnapshot(createInlineTextFormatterSnapshot('c'));
+		testAfterStringifying(inlineText('text').code.text, [ 'c' ]);
 	});
 
 	it('Should support bold formatting', () => {
-		expect(inlineText('text').bold.text).toMatchInlineSnapshot(createInlineTextFormatterSnapshot('b'));
+		testAfterStringifying(inlineText('text').bold.text, [ 'b' ]);
 	});
 
 	it('Should support italic formatting', () => {
-		expect(inlineText('text').italic.text).toMatchInlineSnapshot(createInlineTextFormatterSnapshot('i'));
+		testAfterStringifying(inlineText('text').italic.text, [ 'i' ]);
 	});
 
 	it('Should support underline formatting', () => {
-		expect(inlineText('text').underline.text).toMatchInlineSnapshot(createInlineTextFormatterSnapshot('_'));
+		testAfterStringifying(inlineText('text').underline.text, [ '_' ]);
+	});
+
+	it('Should support highlight formatting', () => {
+		testAfterStringifying(inlineText('text').highlight('red').text, [ 'h', 'red' ]);
+	});
+
+	it('Should support link formatting', () => {
+		testAfterStringifying(inlineText('text').linkTo('www.google.com').text, [ 'a', 'www.google.com' ]);
 	});
 });

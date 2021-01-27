13747436e9fc99da413a71e7e46d933f81657ee3
Safwan Shaheer <devorein00@gmail.com>
2021-01-27 16:19:40 +0600

    TEST+TD(core): Added tds for better block map in test data generation

--- packages/core/tests/types.ts
+++ packages/core/tests/types.ts
@@ -1,11 +1,45 @@
 import {
 	BlockData,
+	IAudio,
+	IBreadcrumb,
+	IBulletedList,
+	ICallout,
+	ICode,
+	ICodepen,
 	ICollection,
+	ICollectionView,
+	ICollectionViewPage,
+	IColumn,
+	IColumnList,
+	IDivider,
+	IDrive,
+	IEmbed,
+	IEquation,
+	IFactory,
+	IFigma,
+	IFile,
+	IGist,
+	IHeader,
+	IImage,
+	ILinkToPage,
+	IMaps,
 	INotionUser,
+	INumberedList,
+	IPage,
+	IQuote,
 	ISpace,
 	ISpaceView,
+	ISubHeader,
+	ISubSubHeader,
+	IText,
+	ITOC,
+	ITodo,
+	IToggle,
+	ITweet,
 	IUserRoot,
 	IUserSettings,
+	IVideo,
+	IWebBookmark,
 	TBlock,
 	TData,
 	TDataType,
@@ -48,6 +82,44 @@ export interface ITestData<D extends TData, T extends TDataType> {
 		incorrect: string;
 	};
 	type: T;
+}
+
+export interface BlockMap {
+	link_to_page: Map<string, ITestData<ILinkToPage, 'block'>[]>;
+	embed: Map<string, ITestData<IEmbed, 'block'>[]>;
+	video: Map<string, ITestData<IVideo, 'block'>[]>;
+	audio: Map<string, ITestData<IAudio, 'block'>[]>;
+	image: Map<string, ITestData<IImage, 'block'>[]>;
+	bookmark: Map<string, ITestData<IWebBookmark, 'block'>[]>;
+	code: Map<string, ITestData<ICode, 'block'>[]>;
+	file: Map<string, ITestData<IFile, 'block'>[]>;
+	tweet: Map<string, ITestData<ITweet, 'block'>[]>;
+	gist: Map<string, ITestData<IGist, 'block'>[]>;
+	codepen: Map<string, ITestData<ICodepen, 'block'>[]>;
+	maps: Map<string, ITestData<IMaps, 'block'>[]>;
+	figma: Map<string, ITestData<IFigma, 'block'>[]>;
+	drive: Map<string, ITestData<IDrive, 'block'>[]>;
+	text: Map<string, ITestData<IText, 'block'>[]>;
+	table_of_contents: Map<string, ITestData<ITOC, 'block'>[]>;
+	equation: Map<string, ITestData<IEquation, 'block'>[]>;
+	breadcrumb: Map<string, ITestData<IBreadcrumb, 'block'>[]>;
+	factory: Map<string, ITestData<IFactory, 'block'>[]>;
+	page: Map<string, ITestData<IPage, 'block'>[]>;
+	to_do: Map<string, ITestData<ITodo, 'block'>[]>;
+	header: Map<string, ITestData<IHeader, 'block'>[]>;
+	sub_header: Map<string, ITestData<ISubHeader, 'block'>[]>;
+	sub_sub_header: Map<string, ITestData<ISubSubHeader, 'block'>[]>;
+	bulleted_list: Map<string, ITestData<IBulletedList, 'block'>[]>;
+	numbered_list: Map<string, ITestData<INumberedList, 'block'>[]>;
+	toggle: Map<string, ITestData<IToggle, 'block'>[]>;
+	quote: Map<string, ITestData<IQuote, 'block'>[]>;
+	divider: Map<string, ITestData<IDivider, 'block'>[]>;
+	callout: Map<string, ITestData<ICallout, 'block'>[]>;
+	collection_view: Map<string, ITestData<ICollectionView, 'block'>[]>;
+	collection_view_page: Map<string, ITestData<ICollectionViewPage, 'block'>[]>;
+	linked_db: Map<string, ITestData<ICollectionView, 'block'>[]>;
+	column_list: Map<string, ITestData<IColumnList, 'block'>[]>;
+	column: Map<string, ITestData<IColumn, 'block'>[]>;
 }
 
 export type TestData = {

4cdee20f750dd59b101c152ad5f04bbc043cc85f
Safwan Shaheer <devorein00@gmail.com>
2021-01-28 10:55:15 +0600

    TEST(nf): Added tests for single number argument number rt for FormulaArray

--- packages/notion-formula/tests/generateASTFromObject.test.ts
+++ packages/notion-formula/tests/generateASTFromObject.test.ts
@@ -2,21 +2,42 @@ import { parseFormulaFromObject } from '../src';
 import deepEqual from 'deep-equal';
 import { generateNumberConstant, generateNumberFunction } from './utils/generateFunction';
 
-describe('Abs method should work correctly', () => {
-	const abs_constant_arg_formula = generateNumberFunction('abs').arg([ generateNumberConstant(1) ]);
+describe('Single number argument formulas with number return type should work correctly', () => {
+  // Single argument number return_type formulas
+  const arg1_nrt_formula_names = [
+		'unaryMinus',
+		'unaryPlus',
+		'abs',
+		'cbrt',
+		'ceil',
+		'exp',
+		'floor',
+		'ln',
+		'log10',
+		'log2',
+		'max',
+		'min',
+		'round',
+		'sign',
+    'sqrt',
+	] as const;
 
-	it('Should work for formula object', () => {
-		expect(
-			deepEqual(
-				abs_constant_arg_formula,
-				parseFormulaFromObject(
-					{
-						function: 'abs',
-						args: 1
-					},
-					new Map()
+	arg1_nrt_formula_names.forEach((arg1_nrt_formula_name) => {
+		it(`Should work for ${arg1_nrt_formula_name} function`, () => {
+			expect(
+				deepEqual(
+					generateNumberFunction(arg1_nrt_formula_name).arg([
+            generateNumberConstant(1)
+          ]),
+          parseFormulaFromObject(
+            {
+              function: arg1_nrt_formula_name,
+              args: 1
+            },
+            new Map()
+          )
 				)
-			)
-		).toBe(true);
+			).toBe(true);
+		});
 	});
 });

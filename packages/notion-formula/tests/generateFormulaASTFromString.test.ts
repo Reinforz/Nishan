b797f91774d867935704c13870c9fdf9a6310c3b
Safwan Shaheer <devorein00@gmail.com>
2021-01-31 11:15:21 +0600

    TEST(nf): Added variadic argument function string parsing

--- packages/notion-formula/tests/generateFormulaASTFromString.test.ts
+++ packages/notion-formula/tests/generateFormulaASTFromString.test.ts
@@ -1,3 +1,4 @@
+import deepEqual from 'deep-equal';
 import { generateFormulaASTFromString } from '../src';
 import { test_schema_map } from './utils';
 
@@ -27,6 +28,40 @@ describe('String function formula parsing error', () => {
 	it('Should throw for improper property argument type', () => {
 		expect(() => generateFormulaASTFromString('abs(prop("text"))')).toThrow(
 			`A property is referenced in the formula, but schema_map argument was not passed`
+		);
+	});
+});
+
+describe('Variadic argument string representation parsing', () => {
+	it(`Should parse correctly when passed correct types`, () => {
+		expect(
+			deepEqual(
+				{
+					name: 'max',
+					type: 'function',
+					args: [
+						{
+							type: 'constant',
+							value: '1',
+							value_type: 'number',
+							result_type: 'number'
+						},
+						{
+							type: 'symbol',
+							name: 'e',
+							result_type: 'number'
+						}
+					],
+					result_type: 'number'
+				},
+				generateFormulaASTFromString('max(1, e)', test_schema_map)
+			)
+		).toBe(true);
+	});
+
+	it('Should throw for improper variadic argument type', () => {
+		expect(() => generateFormulaASTFromString('max("1", e)')).toThrow(
+			`Argument of type text can't be used as argument 1 for function max`
 		);
 	});
 });

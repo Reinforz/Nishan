7d4c11c4b2a8b8cc9aa32e89f959a4530c0cb6cd
Safwan Shaheer <devorein00@gmail.com>
2021-02-04 13:01:26 +0600

    TEST(endpoints): Reached 100% test coverage for queries module

---
+++ packages/endpoints/tests/queries.test.ts
@@ -0,0 +1,62 @@
+import * as queries from '../utils/queries';
+import MockAdapter from 'axios-mock-adapter';
+import axios from 'axios';
+import deepEqual from 'deep-equal';
+
+axios.defaults.baseURL = 'https://www.notion.so/api/v3';
+
+const mock = new MockAdapter(axios);
+
+[
+	'getPageVisits',
+	'getUserSharedPages',
+	'getPublicPageData',
+	'getPublicSpaceData',
+	'getSubscriptionData',
+	'loadBlockSubtree',
+	'getGenericEmbedBlockData',
+	'getUploadFileUrl',
+	'getBacklinksForBlock',
+	'findUser',
+	'syncRecordValues',
+	'queryCollection',
+	'loadPageChunk',
+	'recordPageVisit',
+	'getUserNotifications',
+	'getTasks'
+].forEach((method) => {
+	it(method, async () => {
+		const request_data = {
+				req: 'request_data'
+			},
+			response_data = {
+				res: 'response_data'
+			};
+		mock.onPost(`/${method}`).replyOnce(200, response_data);
+		const response = await (queries as any)[method](request_data, {
+			token: 'token'
+		});
+		expect(deepEqual(response_data, response)).toBe(true);
+	});
+});
+
+[
+	'getUserTasks',
+	'getSpaces',
+	'getGoogleDriveAccounts',
+	'loadUserContent',
+	'getJoinableSpaces',
+	'isUserDomainJoinable',
+	'isEmailEducation'
+].forEach((method) => {
+	it(method, async () => {
+		const response_data = {
+			res: 'response_data'
+		};
+		mock.onPost(`/${method}`).replyOnce(200, response_data);
+		const response = await (queries as any)[method]({
+			token: 'token'
+		});
+		expect(deepEqual(response_data, response)).toBe(true);
+	});
+});

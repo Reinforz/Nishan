d6dd5b789e7ac413b16c920e0e06ceac017e9b3b
Safwan Shaheer <devorein00@gmail.com>
2021-02-08 14:05:11 +0600

    RF(core): Moved createShortId function to generateId module

--- packages/core/utils/ids.ts
+++
@@ -1,6 +0,0 @@
-export function createShortId (length = 5) {
-	let result = '';
-	const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-	for (let i = 0; i < length; i++) result += characters.charAt(Math.floor(Math.random() * characters.length));
-	return result;
-}

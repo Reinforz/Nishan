ab5a132cdfa98d5c22e66d73af1f218ab9508837
Safwan Shaheer <devorein00@gmail.com>
2021-02-08 13:18:28 +0600

    TEST(core): Added tests for generateId module

---
+++ packages/core/tests/unit/generateId.test.ts
@@ -0,0 +1,22 @@
+import { generateId } from '../../src';
+import { v4 as uuidv4 } from 'uuid';
+import colors from 'colors';
+
+describe('generateId', () => {
+	it(`Should generate random id when none provided`, () => {
+		expect(generateId()).toBeTruthy();
+	});
+
+	it(`Should return passed id when correct id is provided`, () => {
+		expect(generateId(uuidv4())).toBeTruthy();
+	});
+
+	it(`Should generate random id when correct id is not provided`, () => {
+		console.log = jest.fn();
+		const generated_id = generateId(uuidv4().slice(1));
+		expect(generated_id).toBeTruthy();
+		expect(console.log).toHaveBeenCalledWith(colors.yellow.bold('Invalid uuid provided'));
+		expect(console.log).toHaveBeenCalledTimes(1);
+		(console.log as jest.Mock<any, any>).mockReset();
+	});
+});

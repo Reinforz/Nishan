9a6ddac7a95cc32121ce213a8974d8d1ebfb5682
Safwan Shaheer <devorein00@gmail.com>
2021-02-08 16:05:55 +0600

    TEST(core): Added tests for logs module

---
+++ packages/core/tests/unit/logs.test.ts
@@ -0,0 +1,24 @@
+import { error, warn } from '../../src';
+import colors from 'colors';
+
+describe('warn', () => {
+	it(`Should log with correct format`, () => {
+		console.log = jest.fn();
+		const message = warn('123');
+		expect(message).toBe('123');
+		expect(console.log).toHaveBeenCalledWith(colors.yellow.bold('123'));
+		expect(console.log).toHaveBeenCalledTimes(1);
+		(console.log as jest.Mock<any, any>).mockReset();
+	});
+});
+
+describe('error', () => {
+	it(`Should log with correct format`, () => {
+		console.log = jest.fn();
+		const message = error('123');
+		expect(message).toBe('123');
+		expect(console.log).toHaveBeenCalledWith(colors.red.bold('123'));
+		expect(console.log).toHaveBeenCalledTimes(1);
+		(console.log as jest.Mock<any, any>).mockReset();
+	});
+});

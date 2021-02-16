9d899e06b734e715d603f3a1f0f8942b9cf8a3bb
Safwan Shaheer <devorein00@gmail.com>
2021-02-16 18:01:15 +0600

    TEST(core): Added tests for all View modules

---
+++ packages/core/tests/View/TimelineView.test.ts
@@ -0,0 +1,23 @@
+import { TimelineView } from '../../src';
+
+it(`TimelineView`, () => {
+	new TimelineView({
+		cache: {
+			block: new Map(),
+			collection: new Map(),
+			collection_view: new Map(),
+			notion_user: new Map(),
+			space: new Map(),
+			space_view: new Map(),
+			user_root: new Map(),
+			user_settings: new Map()
+		},
+		id: 'collection_view_1',
+		interval: 0,
+		shard_id: 123,
+		space_id: 'space_1',
+		stack: [],
+		token: 'token',
+		user_id: 'user_1'
+	});
+});

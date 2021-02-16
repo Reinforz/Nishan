3d59ed6d5980312984d796e47bd49322d8a6ba1b
Safwan Shaheer <devorein00@gmail.com>
2021-02-16 09:50:30 +0600

    TEST(core): Added tests for PopulateMap module

--- packages/core/utils/PopulateMap.ts
+++ packages/core/utils/PopulateMap.ts
@@ -1,19 +1,26 @@
 import { NotionCacheObject } from '@nishans/cache';
-import { ICollection, TBlock, TPage } from '@nishans/types';
+import { ICollection, TBlock, TCollectionBlock, TPage } from '@nishans/types';
+import { CollectionView, CollectionViewPage } from '../src';
 import { IBlockMap, IPageMap, NishanArg } from '../types';
 import { createBlockClass } from '../utils';
+
+async function populateCollectionBlockMap (
+	block: TCollectionBlock,
+	props: Omit<NishanArg, 'id'>,
+	block_obj: CollectionView | CollectionViewPage,
+	block_map: IBlockMap
+) {
+	await NotionCacheObject.initializeCacheForSpecificData(block.id, 'block', props, props.cache);
+	const collection = props.cache.collection.get(block.collection_id) as ICollection;
+	block_map[block.type].set(collection.name[0][0], block_obj as any);
+}
 
 async function populatePageMap (page: TPage, page_map: IPageMap, props: Omit<NishanArg, 'id'>) {
 	const block_obj = createBlockClass(page.type, page.id, props);
 	page_map[page.type].set(page.id, block_obj);
 
-	if (page.type === 'page') {
-		page_map.page.set(page.properties.title[0][0], block_obj);
-	} else if (page.type === 'collection_view_page') {
-		await NotionCacheObject.initializeCacheForSpecificData(page.id, 'block', props, props.cache);
-		const collection = props.cache.collection.get(page.collection_id) as ICollection;
-		page_map.collection_view_page.set(collection.name[0][0], block_obj);
-	}
+	if (page.type === 'page') page_map.page.set(page.properties.title[0][0], block_obj);
+	else await PopulateMap.collection_block(page, props, block_obj, page_map as any);
 }
 
 async function populateBlockMap (block: TBlock, block_map: IBlockMap, props: Omit<NishanArg, 'id'>) {
@@ -22,15 +29,12 @@ async function populateBlockMap (block: TBlock, block_map: IBlockMap, props: Omi
 	} else {
 		const block_obj = createBlockClass(block.type, block.id, props);
 		block_map[block.type].set(block.id, block_obj);
-		if (block.type === 'collection_view') {
-			await NotionCacheObject.initializeCacheForSpecificData(block.id, 'block', props, props.cache);
-			const collection = props.cache.collection.get(block.collection_id) as ICollection;
-			block_map.collection_view_page.set(collection.name[0][0], block_obj);
-		}
+		if (block.type === 'collection_view') await PopulateMap.collection_block(block, props, block_obj, block_map);
 	}
 }
 
 export const PopulateMap = {
 	page: populatePageMap,
-	block: populateBlockMap
+	block: populateBlockMap,
+	collection_block: populateCollectionBlockMap
 };

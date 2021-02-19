51f0cea098bdc33824b84a57d85d837eec39ecef
Safwan Shaheer <devorein00@gmail.com>
2021-02-19 11:01:25 +0600

    FEAT(ops): Made stack private and created pushToStack method

--- packages/operations/src/OperationsClass.ts
+++ packages/operations/src/OperationsClass.ts
@@ -3,33 +3,33 @@ import { IOperation } from '@nishans/types';
 import { NotionOperationPluginFunction } from './types';
 
 export class NotionOperationsClass {
-	stack: IOperation[] = [];
+	#stack: IOperation[] = [];
+  #plugins: NotionOperationPluginFunction[];
 	space_id: string;
 	shard_id: number;
 	token: string;
-  #plugins: NotionOperationPluginFunction[];
 
 	constructor (args: { token: string; space_id: string; shard_id: number; stack: IOperation[], plugins?: NotionOperationPluginFunction[] }) {
 		this.space_id = args.space_id;
 		this.shard_id = args.shard_id;
-		this.stack = args.stack;
 		this.token = args.token;
+		this.#stack = args.stack;
     this.#plugins = args.plugins ?? []; 
 	}
 
 	emptyStack () {
-		while (this.stack.length !== 0) this.stack.pop();
+		while (this.#stack.length !== 0) this.#stack.pop();
 	}
 
 	printStack () {
-		console.log(JSON.stringify(this.stack, null, 2));
+		console.log(JSON.stringify(this.#stack, null, 2));
 	}
 
   applyPluginsToOperationsStack(){
     if(this.#plugins.length !== 0 ){
       const updated_operations: IOperation[] = [];
-      for (let index = 0; index < this.stack.length; index++) {
-        const operation = this.stack[index];
+      for (let index = 0; index < this.#stack.length; index++) {
+        const operation = this.#stack[index];
         let updated_operation: false | IOperation = operation;
         for (let index = 0; index < this.#plugins.length; index++) {
           const plugin = this.#plugins[index];
@@ -42,13 +42,18 @@ export class NotionOperationsClass {
       }
       return updated_operations;
     }else
-      return this.stack
+      return this.#stack
+  }
+
+  pushToStack(operations: IOperation | IOperation[]){
+    if(Array.isArray(operations)) this.#stack.push(...operations)
+    else this.#stack.push(operations);
   }
 
 	async executeOperation () {
-		if (this.stack.length === 0) console.log(`The operation stack is empty`);
+		if (this.#stack.length === 0) console.log(`The operation stack is empty`);
 		else {
-      const created_transaction = createTransaction(this.shard_id, this.space_id, this.stack);
+      const created_transaction = createTransaction(this.shard_id, this.space_id, this.#stack);
       created_transaction.transactions[0].operations = this.applyPluginsToOperationsStack();
 			await Mutations.saveTransactions(created_transaction, {
 				token: this.token,

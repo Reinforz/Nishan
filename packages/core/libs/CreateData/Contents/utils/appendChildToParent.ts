a7d990c03812340e58d59b567d354bd8b25efd3a
Safwan Shaheer <devorein00@gmail.com>
2021-02-22 11:58:32 +0600

    RF(core): Refactored appendChildToParent util method

--- packages/core/libs/CreateData/Contents/utils/appendChildToParent.ts
+++ packages/core/libs/CreateData/Contents/utils/appendChildToParent.ts
@@ -1,7 +1,20 @@
 import { ICache, NotionCacheObject } from '@nishans/cache';
 import { Operation } from '@nishans/operations';
-import { ICollection, IOperation, IPage, ISpace } from '@nishans/types';
+import { IOperation } from '@nishans/types';
+import { detectChildData } from '../../../detectChildData';
 
+/**
+ * 1. Fetches parent from notion's db if it doesn't exist in cache
+ * 2. Detect the child data using the parent
+ * 3. Push the child id to the parent's child container
+ * 4. Push a suitable operation to the operation stack for adding the child to the child container
+ * @param parent_table Parent data table
+ * @param parent_id Parent data id
+ * @param content_id Child id
+ * @param cache Cache to lookup parent
+ * @param stack Stack to push operation
+ * @param token Token required to fetching parent if not present in cache
+ */
 export async function appendChildToParent (
 	parent_table: 'space' | 'block' | 'collection',
 	parent_id: string,
@@ -10,42 +23,18 @@ export async function appendChildToParent (
 	stack: IOperation[],
 	token: string
 ): Promise<void> {
-	switch (parent_table) {
-		case 'block': {
-			stack.push(Operation.block.listAfter(parent_id, [ 'content' ], { after: '', id: content_id }));
-			const parent = await NotionCacheObject.fetchDataOrReturnCached<IPage>(
-				parent_table,
-				parent_id,
-				{ token, interval: 0 },
-				cache
-			);
-			if (!parent['content']) parent['content'] = [];
-			parent['content'].push(content_id);
-			break;
-		}
-		case 'space': {
-			stack.push(Operation.space.listAfter(parent_id, [ 'pages' ], { after: '', id: content_id }));
-			const parent = await NotionCacheObject.fetchDataOrReturnCached<ISpace>(
-				parent_table,
-				parent_id,
-				{ token, interval: 0 },
-				cache
-			);
-			if (!parent['pages']) parent['pages'] = [];
-			parent['pages'].push(content_id);
-			break;
-		}
-		case 'collection': {
-			stack.push(Operation.collection.listAfter(parent_id, [ 'template_pages' ], { after: '', id: content_id }));
-			const parent = await NotionCacheObject.fetchDataOrReturnCached<ICollection>(
-				parent_table,
-				parent_id,
-				{ token, interval: 0 },
-				cache
-			);
-			if (!parent['template_pages']) parent['template_pages'] = [];
-			parent['template_pages'].push(content_id);
-			break;
-		}
-	}
+	// Fetches parent from notion's db if it doesn't exist in cache
+	const parent = await NotionCacheObject.fetchDataOrReturnCached(
+		parent_table,
+		parent_id,
+		{ token, interval: 0 },
+		cache
+	);
+	// Detect the child data using the parent
+	const [ child_path ] = detectChildData(parent_table as any, parent as any);
+	// Push the child id to the parent's child container
+	if (!(parent as any)[child_path]) (parent as any)[child_path] = [];
+	(parent as any)[child_path].push(content_id);
+	// Push a suitable operation to the operation stack for adding the child to the child container
+	stack.push(Operation[parent_table].listAfter(parent_id, [ child_path ], { after: '', id: content_id }));
 }

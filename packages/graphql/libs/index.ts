c2c811115c7859b3da3cf8a8a8cf8963986a311b
Safwan Shaheer <devorein00@gmail.com>
2021-03-05 01:22:48 +0600

    FEAT(gql): Added feature to get cvp from space

--- packages/graphql/libs/index.ts
+++ packages/graphql/libs/index.ts
@@ -1,25 +1,32 @@
 require('dotenv').config({ path: '../../.env' });
 import { NotionQueries, NotionRequestConfigs } from '@nishans/endpoints';
-import { IPage, ISpace, TDataType } from '@nishans/types';
+import { IPage, ISpace, TDataType, TPage } from '@nishans/types';
 import { ApolloServer, gql } from 'apollo-server';
 
 // The GraphQL schema
 const typeDefs = gql`
+  
+  union TPage = Page | CollectionViewPage
+
 	type Space {
-		id: String
-		name: String
-		pages: [Page]
+		id: String!
+		name: String!
+		pages: [TPage!]
 	}
 
 	type PageProperties {
 		title: [[String]]
 	}
 
+  type CollectionViewPage {
+    id: String!
+  }
+
 	type Page {
-		properties: PageProperties
-		id: String
-		type: String
-		parent: Space
+		properties: PageProperties!
+		id: String!
+		type: String!
+		parent: Space!
 	}
 
 	type Query {
@@ -50,17 +57,14 @@ async function fetchNotionData(id: string, table: TDataType, request_configs: No
 // A map of functions which return data for the schema.
 const resolvers = {
 	Query: {
-    space: async (_: any, args: { id: string }, ctx: NotionRequestConfigs) => {
-      return await fetchNotionData(args.id, 'space', ctx);
-    },
-		page: async (_: any, args: { id: string }, ctx: NotionRequestConfigs) => {
-      return await fetchNotionData(args.id, 'block', ctx);
-		}
+    space: async (_: any, args: { id: string }, ctx: NotionRequestConfigs) =>
+      await fetchNotionData(args.id, 'space', ctx),
+		page: async (_: any, args: { id: string }, ctx: NotionRequestConfigs) =>
+      await fetchNotionData(args.id, 'block', ctx)
 	},
 	Page: {
-		parent: async ({ parent_id }: IPage, _: any, ctx: NotionRequestConfigs) => {
-      return await fetchNotionData(parent_id, 'space', ctx);
-		}
+		parent: async ({ parent_id }: IPage, _: any, ctx: NotionRequestConfigs) => 
+      await fetchNotionData(parent_id, 'space', ctx)
 	},
 	Space: {
 		pages: async ({ pages: page_ids }: ISpace, _: any, ctx: NotionRequestConfigs) => {
@@ -85,7 +89,13 @@ const resolvers = {
 			}
 			return pages;
 		}
-	}
+	},
+  TPage: {
+    __resolveType: (obj: TPage) => {
+      if(obj.type === "page") return "Page";
+      else if(obj.type === "collection_view_page") return "CollectionViewPage";
+    }
+  }
 };
 
 const server = new ApolloServer({

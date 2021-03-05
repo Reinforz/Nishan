f98b00e83af2c16b46e3835f56302e22145cf747
Safwan Shaheer <devorein00@gmail.com>
2021-03-05 14:05:37 +0600

    FEAT(gql): server factory takes in token,interval and user_id as param

--- packages/graphql/libs/NotionGraphql/server.ts
+++ packages/graphql/libs/NotionGraphql/server.ts
@@ -1,19 +1,20 @@
 import { Nishan } from '@nishans/core';
+import { NotionRequestConfigs } from '@nishans/endpoints';
 import { ApolloServer } from 'apollo-server';
-import { NotionGraphqlServerResolvers } from "./Resolvers";
-import { NotionGraphqlServerTypedefs } from "./typedefs";
+import { NotionGraphqlServerResolvers } from './Resolvers';
+import { NotionGraphqlServerTypedefs } from './typedefs';
 
-export const server = async () => {
-  const nishan = new Nishan({
-    token: process.env.NISHAN_NOTION_TOKEN_V2 as string,
-    interval: parseInt(process.env.NISHAN_NOTION_REQUEST_INTERVAL ?? "0") as number
-  });
-  
-  const notion_user = await nishan.getNotionUser(process.env.NISHAN_NOTION_USER_ID as string);
+export const server = async ({ token, interval, user_id }: Required<NotionRequestConfigs>) => {
+	const nishan = new Nishan({
+		token,
+		interval
+	});
 
-  return new ApolloServer({
-    typeDefs: NotionGraphqlServerTypedefs,
-    resolvers: NotionGraphqlServerResolvers,
-    context: () => notion_user.getProps()
-  })
+	const notion_user = await nishan.getNotionUser(user_id);
+
+	return new ApolloServer({
+		typeDefs: NotionGraphqlServerTypedefs,
+		resolvers: NotionGraphqlServerResolvers,
+		context: () => notion_user.getProps()
+	});
 };

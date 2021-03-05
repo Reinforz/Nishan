2f9eaa61341dad81ccb9cd0fa60bc85e75727a7e
Safwan Shaheer <devorein00@gmail.com>
2021-03-05 20:01:52 +0600

    TEST(cache): Added tests for fetchMultipleDataOrReturnCached

--- packages/cache/tests/NotionCacheObject.test.ts
+++ packages/cache/tests/NotionCacheObject.test.ts
@@ -654,3 +654,50 @@ describe('validateCache', () => {
 		).toBeTruthy();
 	});
 });
+
+describe('fetchMultipleDataOrReturnCached', () => {
+	it(`works correctly`, async () => {
+		const block_1 = {
+				id: 'block_1'
+			},
+			block_2 = {
+				id: 'block_2'
+			},
+			cache: ICache = {
+				block: new Map([ [ 'block_1', block_1 ] ])
+			} as any;
+		const syncRecordValuesMock = jest.spyOn(NotionQueries, 'syncRecordValues');
+		syncRecordValuesMock.mockImplementationOnce(async () => {
+			return {
+				recordMap: {
+					block: {
+						block_2: {
+							value: block_2
+						}
+					}
+				}
+			} as any;
+		});
+		const result_record_map = await NotionCacheObject.fetchMultipleDataOrReturnCached(
+			[ [ 'block_1', 'block' ], [ 'block_2', 'block' ] ],
+			notion_request_configs,
+			cache
+		);
+		expect(result_record_map.block[0]).toBe(block_1);
+		expect(result_record_map.block[1]).toBe(block_2);
+		expect(syncRecordValuesMock).toHaveBeenCalledWith(
+			{
+				requests: [
+					{
+						id: 'block_2',
+						table: 'block',
+						version: 0
+					}
+				]
+			},
+			notion_request_configs
+		);
+		expect(cache.block.get('block_1')).toStrictEqual(block_1);
+		expect(cache.block.get('block_2')).toStrictEqual(block_2);
+	});
+});

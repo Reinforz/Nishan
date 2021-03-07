6c94043ac64183c8c32732ce32c43695d4c3e5d0
Safwan Shaheer <devorein00@gmail.com>
2021-03-07 18:26:55 +0600

    RF(gql): Refactored Space.pages resolver

--- packages/graphql/libs/Resolvers/space.ts
+++ packages/graphql/libs/Resolvers/space.ts
@@ -1,30 +1,9 @@
-import { INotionCacheOptions } from '@nishans/cache';
-import { NotionEndpoints } from '@nishans/endpoints';
-import { IPage, ISpace } from '@nishans/types';
+import { INotionCacheOptions, NotionCache } from '@nishans/cache';
+import { ISpace } from '@nishans/types';
 import { NotionGraphqlNotionUserResolvers } from './utils';
 
 export const NotionGraphqlSpaceResolver = {
-	pages: async ({ pages: page_ids }: ISpace, _: any, ctx: INotionCacheOptions) => {
-		const { recordMap } = await NotionEndpoints.Queries.syncRecordValues(
-			{
-				requests: page_ids.map((page_id) => ({
-					id: page_id,
-					table: 'block',
-					version: 0
-				})) as any
-			},
-			{
-				interval: ctx.interval,
-				token: ctx.token,
-				user_id: ctx.user_id
-			}
-		);
-
-		const pages: IPage[] = [];
-		for (const page_id in recordMap.block) {
-			pages.push(recordMap.block[page_id].value as IPage);
-		}
-		return pages;
-	},
+	pages: async (parent: ISpace, _: any, ctx: INotionCacheOptions) =>
+		(await NotionCache.fetchMultipleDataOrReturnCached(parent.pages.map((id) => [ id, 'block' ]), ctx)).block,
 	...NotionGraphqlNotionUserResolvers
 };

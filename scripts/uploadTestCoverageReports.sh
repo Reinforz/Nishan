c5b9dc337e2be74e84a98da57690a7841eb959c3
Safwan Shaheer <devorein00@gmail.com>
2021-03-13 19:39:11 +0600

    FIX(script): Fixed uploadTestCoverageReports script

--- scripts/uploadTestCoverageReports.sh
+++ scripts/uploadTestCoverageReports.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 CODECOV_SCRIPT="${GITHUB_WORKSPACE}/scripts/codecov.sh"
-FILES=""
 
 curl -s https://codecov.io/bash > $CODECOV_SCRIPT
 chmod +x $CODECOV_SCRIPT
@@ -12,17 +11,6 @@ for dir in */
   do
     if [ -d "${dir}coverage" ]
     then
-      FILENAME="$PWD/${dir}coverage/lcov.info"
-      if [ -z $FILES ]
-      then
-        FILES=$FILENAME
-      else
-        FILES="${FILES},$FILENAME"
-      fi
+      $codecov_file -f "$PWD/$package/coverage/lcov.info" -v -t $CODECOV_TOKEN
     fi
   done
-
-if [ ! -z "$FILES" ]
-then
-  $CODECOV_SCRIPT -f $FILES -v -t $CODECOV_TOKEN
-fi

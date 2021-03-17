c883c7e7db556b91dc620ab98505235860f6f2be
Safwan Shaheer <devorein00@gmail.com>
2021-03-17 23:00:08 +0600

    DEPS(*): Updated and published packages

--- scripts/libs/Build/buildAfterTest.ts
+++ scripts/libs/Build/buildAfterTest.ts
@@ -1,18 +1,20 @@
 import cp from 'child_process';
 import colors from 'colors';
+import fs from 'fs';
 import path from 'path';
 
-export function buildAfterTest (packages: string[]) {
+export async function buildAfterTest (packages: string[]) {
 	const packages_dir = path.resolve(__dirname, '../../../../packages');
 	for (let index = 0; index < packages.length; index++) {
 		const package_name = packages[index].split('/')[1];
 		const package_dir = path.join(packages_dir, package_name);
 		console.log(colors.green.bold(`Building ${packages[index]}`));
-		console.log(package_dir);
-
+    const package_json_data = JSON.parse(await fs.promises.readFile(path.join(package_dir, 'package.json'), 'utf-8'))
 		try {
-			cp.execSync(`npm run test`, { cwd: package_dir });
-			console.log(colors.green.bold(`Test completed`));
+      if(package_json_data?.scripts?.test){
+        cp.execSync(`npm run test`, { cwd: package_dir });
+        console.log(colors.green.bold(`Test completed`));
+      }
 			cp.execSync(`npx del-cli ./dist`, { cwd: package_dir });
 			console.log(colors.green.bold(`Deleted dist folder`));
 			cp.execSync(`npm run build`, { cwd: package_dir });
--- scripts/libs/Publish/publishUpdatedPackages.ts
+++ scripts/libs/Publish/publishUpdatedPackages.ts
@@ -1,3 +1,4 @@
+import colors from 'colors';
 import { buildAfterTest } from '../Build/buildAfterTest';
 import { createDependencyMap } from '../Create/createDependencyMap';
 import { createPackageMap } from '../Create/createPackageMap';
@@ -9,7 +10,9 @@ export async function publishUpdatedPackages (updated_packages_name: string[]) {
 	const packages_map = await createPackageMap(),
 		package_dependency_map = createDependencyMap(updated_packages_name, packages_map),
 		rearranged_packages = createPackagePublishOrder(Array.from(package_dependency_map.all.keys()), packages_map);
+	console.log(colors.blue.bold(`Publishing packages\n\n${rearranged_packages.join('\n')}`));
+
 	const rearranged_packages_map = updatePatchVersion(rearranged_packages, packages_map, 1);
-	buildAfterTest(rearranged_packages);
+	await buildAfterTest(rearranged_packages);
 	await publishPackages(rearranged_packages_map);
 }

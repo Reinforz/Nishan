4325cf4b8007a5f54175c629a84576c01b8e550e
Safwan Shaheer <devorein00@gmail.com>
2021-03-21 22:41:03 +0600

    FEAT(dis): Added restore comments method

--- packages/discourse/libs/Comments/index.ts
+++ packages/discourse/libs/Comments/index.ts
@@ -1,9 +1,11 @@
 import { createComments } from './createComments';
 import { deleteComments } from './deleteComments';
+import { restoreComments } from './restoreComments';
 import { updateComments } from './updateComments';
 
 export const NotionDiscourseComments = {
 	create: createComments,
 	update: updateComments,
-  delete: deleteComments
+	delete: deleteComments,
+	restore: restoreComments
 };
---
+++ packages/discourse/libs/Comments/restoreComments.ts
@@ -0,0 +1,31 @@
+import { INotionCacheOptions, NotionCache } from '@nishans/cache';
+import { INotionOperationOptions, NotionOperations } from '@nishans/operations';
+import { IComment, IDiscussion, IOperation } from '@nishans/types';
+import { NotionUtils } from '@nishans/utils';
+
+export const restoreComments = async (ids: string[], options: INotionCacheOptions & INotionOperationOptions) => {
+	const operations: IOperation[] = [];
+	for (let index = 0; index < ids.length; index++) {
+		const id = ids[index];
+		const comment_data = (await NotionCache.fetchDataOrReturnCached('comment', id, options)) as IComment;
+		const discussion_data = (await NotionCache.fetchDataOrReturnCached(
+			'discussion',
+			comment_data.parent_id,
+			options
+		)) as IDiscussion;
+		comment_data.alive = true;
+		discussion_data.comments.push(comment_data.id);
+		operations.push(
+			NotionOperations.Chunk.comment.set(comment_data.id, [ 'alive' ], true),
+			NotionOperations.Chunk.discussion.listAfter(discussion_data.id, [ 'comments' ], {
+				id: comment_data.id
+			}),
+			NotionOperations.Chunk.comment.update(
+				comment_data.id,
+				[],
+				NotionUtils.updateLastEditedProps(comment_data, options.user_id)
+			)
+		);
+	}
+	await NotionOperations.executeOperations(operations, options);
+};

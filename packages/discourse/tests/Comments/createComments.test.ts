1d904dd5fd3f8f447fbe77323ffa651a0e0638f3
Safwan Shaheer <devorein00@gmail.com>
2021-03-21 21:45:24 +0600

    TEST(dis): Added tests for comment methods

---
+++ packages/discourse/tests/Comments/createComments.test.ts
@@ -0,0 +1,53 @@
+import { NotionCache } from '@nishans/cache';
+import { NotionIdz } from '@nishans/idz';
+import { NotionOperations } from '@nishans/operations';
+import { default_nishan_arg, o } from '../../../core/tests/utils';
+import { NotionDiscourse } from '../../libs';
+
+it(`NotionDiscourse.createComments`, async () => {
+	const discussion_data: any = { id: 'discussion_1' },
+		cache = {
+			...NotionCache.createDefaultCache(),
+			discussion: new Map([ [ 'discussion_1', discussion_data ] ])
+		},
+		options = {
+			...default_nishan_arg,
+			cache
+		},
+		comment_id = NotionIdz.Generate.id(),
+		executeOperationsMock = jest.spyOn(NotionOperations, 'executeOperations').mockImplementation(async () => undefined),
+		comment_1_data = {
+			parent_id: expect.any(String),
+			parent_table: 'discussion',
+			text: [ [ 'First Comment' ] ],
+			alive: true,
+			id: comment_id,
+			version: 1,
+			space_id: options.space_id,
+			shard_id: options.shard_id,
+			created_by_id: options.user_id,
+			created_by_table: 'notion_user',
+			last_edited_by_id: options.user_id,
+			last_edited_by_table: 'notion_user'
+		};
+
+	await NotionDiscourse.Comments.create(
+		[
+			{
+				discussion_id: 'discussion_1',
+				text: [ [ 'First Comment' ] ],
+				comment_id
+			}
+		],
+		options
+	);
+
+	expect(executeOperationsMock.mock.calls[0][0]).toStrictEqual([
+		o.cm.u(comment_id, [], expect.objectContaining(comment_1_data)),
+		o.d.la('discussion_1', [ 'comments' ], {
+			id: comment_id
+		})
+	]);
+	expect(cache.comment.get(comment_id)).toStrictEqual(expect.objectContaining(comment_1_data));
+	expect(discussion_data.comments).toStrictEqual([ comment_id ]);
+});
---
+++ packages/discourse/tests/Comments/updateComments.test.ts
@@ -0,0 +1,34 @@
+import { NotionCache } from '@nishans/cache';
+import { NotionIdz } from '@nishans/idz';
+import { NotionOperations } from '@nishans/operations';
+import { default_nishan_arg, o } from '../../../core/tests/utils';
+import { NotionDiscourse } from '../../libs';
+
+it(`NotionDiscourse.updateComments`, async () => {
+	const comment_id = NotionIdz.Generate.id(),
+		comment_data: any = { id: comment_id },
+		cache = {
+			...NotionCache.createDefaultCache(),
+			comment: new Map([ [ comment_id, comment_data ] ])
+		},
+		options = {
+			...default_nishan_arg,
+			cache
+		},
+		executeOperationsMock = jest.spyOn(NotionOperations, 'executeOperations').mockImplementation(async () => undefined);
+
+	await NotionDiscourse.Comments.update(
+		[
+			{
+				comment_id,
+				text: [ [ 'New Comment' ] ]
+			}
+		],
+		options
+	);
+
+	expect(executeOperationsMock.mock.calls[0][0]).toStrictEqual([
+		o.cm.s(comment_id, [ 'text' ], [ [ 'New Comment' ] ])
+	]);
+	expect(comment_data.text).toStrictEqual([ [ 'New Comment' ] ]);
+});

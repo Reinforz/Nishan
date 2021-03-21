6fc8a0ce5ab4b4e8b8cb4e918ce3158b3f043d80
Safwan Shaheer <devorein00@gmail.com>
2021-03-21 22:34:46 +0600

    TEST(dis): Added test for deleteComments method

---
+++ packages/discourse/tests/Comments/deleteComments.test.ts
@@ -0,0 +1,33 @@
+import { NotionCache } from '@nishans/cache';
+import { NotionIdz } from '@nishans/idz';
+import { NotionOperations } from '@nishans/operations';
+import { default_nishan_arg, last_edited_props, o } from '../../../core/tests/utils';
+import { NotionDiscourse } from '../../libs';
+
+it(`NotionDiscourse.deleteComments`, async () => {
+	const comment_id = NotionIdz.Generate.id(),
+		comment_data: any = { id: comment_id, parent_id: 'discussion_1' },
+		discussion_data: any = { comments: [ comment_id ], id: 'discussion_1' },
+		cache = {
+			...NotionCache.createDefaultCache(),
+			comment: new Map([ [ comment_id, comment_data ] ]),
+			discussion: new Map([ [ 'discussion_1', discussion_data ] ])
+		},
+		options = {
+			...default_nishan_arg,
+			cache
+		},
+		executeOperationsMock = jest.spyOn(NotionOperations, 'executeOperations').mockImplementation(async () => undefined);
+
+	await NotionDiscourse.Comments.delete([ comment_id ], options);
+
+	expect(executeOperationsMock.mock.calls[0][0]).toStrictEqual([
+		o.cm.s(comment_id, [ 'alive' ], false),
+		o.d.lr('discussion_1', [ 'comments' ], {
+			id: comment_id
+		}),
+		o.cm.u(comment_id, [], last_edited_props)
+	]);
+	expect(comment_data.alive).toBe(false);
+	expect(discussion_data.comments).toStrictEqual([]);
+});

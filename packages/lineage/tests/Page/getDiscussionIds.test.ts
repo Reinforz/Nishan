7c3315fba80724cb70095c521b0d1bf8d8fcd9f3
Safwan Shaheer <devorein00@gmail.com>
2021-03-22 18:55:58 +0600

    TEST(lin): Added tests for getDiscussionIds

---
+++ packages/lineage/tests/Page/getDiscussionIds.test.ts
@@ -0,0 +1,15 @@
+import { NotionCache } from '@nishans/cache';
+import { NotionLineage } from '../../libs';
+
+it(`NotionLineage.Page.getDiscussionIds`, () => {
+	const block_1: any = { content: [ 'block_2', 'block_3' ] },
+		block_2: any = { discussions: [ 'discussion_1' ] },
+		block_3: any = {},
+		cache = {
+			...NotionCache.createDefaultCache(),
+			block: new Map([ [ 'block_1', block_1 ], [ 'block_2', block_2 ], [ 'block_3', block_3 ] ])
+		};
+
+	const discussion_ids = NotionLineage.Page.getDiscussionIds(block_1, cache);
+	expect(discussion_ids).toStrictEqual([ 'discussion_1' ]);
+});

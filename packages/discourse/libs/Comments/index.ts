0e46ed3f5c3d57c7190e6dc40e9b1e4813c38eed
Safwan Shaheer <devorein00@gmail.com>
2021-03-26 20:49:04 +0600

    FEAT(dis): removed restoreComments from index

--- packages/discourse/libs/Comments/index.ts
+++ packages/discourse/libs/Comments/index.ts
@@ -1,11 +1,9 @@
 import { createComments } from './createComments';
 import { deleteComments } from './deleteComments';
-import { restoreComments } from './restoreComments';
 import { updateComments } from './updateComments';
 
 export const NotionDiscourseComments = {
 	create: createComments,
 	update: updateComments,
-	delete: deleteComments,
-	restore: restoreComments
+	delete: deleteComments
 };

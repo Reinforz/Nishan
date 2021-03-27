4df8e6862bf74a6530fb3a53718bdbe83c66a381
Safwan Shaheer <devorein00@gmail.com>
2021-03-27 23:05:16 +0600

    FIX(core): Added populateViewMap module

---
+++ packages/core/libs/PopulateMap/populateViewMap.ts
@@ -0,0 +1,22 @@
+import { INotionFabricatorOptions } from '@nishans/fabricator';
+import { TView } from '@nishans/types';
+import { IViewMap } from '../../types';
+import { BoardView, CalendarView, GalleryView, ListView, TableView, TimelineView } from '../api/View';
+
+const ViewClass = {
+	board: BoardView,
+	gallery: GalleryView,
+	list: ListView,
+	timeline: TimelineView,
+	table: TableView,
+	calendar: CalendarView
+};
+
+export const populateViewMap = (view: TView, options: INotionFabricatorOptions, view_map: IViewMap) => {
+	const view_obj = new ViewClass[view.type]({
+		id: view.id,
+		...options
+	}) as any;
+	view_map[view.type].set(view.id, view_obj);
+	view_map[view.type].set(view.name, view_obj);
+};

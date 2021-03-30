736b6f55c4f7764870db007e0a6c17f97853e089
Safwan Shaheer <devorein00@gmail.com>
2021-03-30 19:59:55 +0600

    FEAT(scripts): Created basic styling

--- scripts/client/src/PackageList/index.tsx
+++ scripts/client/src/PackageList/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import type { IPackageInfo } from '..//App';
+import type { IPackageInfo } from '../App';
+import "./index.css";
 
 interface Props {
   packages: IPackageInfo[],
@@ -8,14 +9,14 @@ interface Props {
 
 export function PackageList({ packages, setPackages }: Props) {
   return (
-    <div>
+    <div className="PackageList">
       {packages.map((package_data) =>
-        <div key={package_data.name}>
-          <input type="checkbox" value={package_data.checked as any} onChange={(e) => {
+        <div key={package_data.name} className="PackageList-item">
+          <input className="PackageList-item-checkbox" type="checkbox" value={package_data.checked as any} onChange={(e) => {
             package_data.checked = e.target.checked;
             setPackages(packages);
           }} />
-          <span className="PackageList-name">{package_data.name}</span>
+          <span className="PackageList-item-name">{package_data.name}</span>
         </div>
       )}
     </div>
--- scripts/client/src/PackageStatus/index.tsx
+++ scripts/client/src/PackageStatus/index.tsx
@@ -13,8 +13,8 @@ export function PackageStatus({ packages_status }: Props) {
         <div key={packages_status.name} className="PackageStatus-item">
           <div className="PackageStatus-item-name">{packages_status.name}</div>
           <div className="PackageStatus-item-steps">
-            {packages_status.steps.map(package_status_step => <span className="PackageStatus-item-step">
-              {package_status_step.step}
+            {packages_status.steps.map((_, index) => <span className="PackageStatus-item-step">
+              {index + 1}
             </span>)}
           </div>
         </div>

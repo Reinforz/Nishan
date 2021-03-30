99b3ade0382cdb32ad85f39a657454e57947236b
Safwan Shaheer <devorein00@gmail.com>
2021-03-30 10:50:10 +0600

    FEAT(scripts): removed build folder and added to gitignore

--- scripts/client/build/_snowpack/pkg/react.js
+++
@@ -1,2 +0,0 @@
-import { r as react } from './common/index-ae389540.js';
-export { r as default } from './common/index-ae389540.js';
--- scripts/client/build/index.js
+++
@@ -1,3 +0,0 @@
-import React from "./_snowpack/pkg/react.js";
-import ReactDOM from "./_snowpack/pkg/react-dom.js";
-ReactDOM.render(/* @__PURE__ */ React.createElement("div", null, "HELLO FROM REACT"), document.getElementById("root"));

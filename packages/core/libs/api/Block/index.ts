cefbf817945526c9dec5f00fe191653461cacc48
Safwan Shaheer <devorein00@gmail.com>
2021-04-08 23:51:00 +0600

    FIX(core):Duplicate api folder

--- packages/core/libs/api/Block/index.ts
+++
@@ -1,7 +0,0 @@
-import Block from './Block';
-import CollectionBlock from './CollectionBlock';
-import CollectionView from './CollectionView';
-import CollectionViewPage from './CollectionViewPage';
-import Page from './Page';
-
-export { Block, CollectionViewPage, CollectionView, Page, CollectionBlock };

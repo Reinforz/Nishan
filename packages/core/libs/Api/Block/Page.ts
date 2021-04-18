9e16cdc124d8dfdc7f64d7a5431b5f16beb9cb38
Safwan Shaheer <devorein00@gmail.com>
2021-04-18 17:47:37 +0600

    RF(core): Removed PageBlock methods from Page class

--- packages/core/libs/Api/Block/Page.ts
+++ packages/core/libs/Api/Block/Page.ts
@@ -1,59 +1,22 @@
-import { NotionCache } from '@nishans/cache';
 import { NotionFabricator, TBlockCreateInput, TBlockInput } from '@nishans/fabricator';
-import { NotionLineage } from '@nishans/lineage';
-import { NotionOperations } from '@nishans/operations';
 import { NotionPermissions } from '@nishans/permissions';
 import { FilterType, FilterTypes, UpdateType, UpdateTypes } from '@nishans/traverser';
-import { IPage, ISpace, ISpaceView, TBlock } from '@nishans/types';
+import { TBlock } from '@nishans/types';
 import { IBlockMap, INotionCoreOptions, NotionCore } from '../../';
 import { transformToMultiple } from '../../utils';
-import Block from './Block';
+import PageBlock from './PageBlock';
 
 /**
  * A class to represent Page type block of Notion
  * @noInheritDoc
  */
 
-export default class Page extends Block<IPage, Partial<Pick<IPage, 'properties' | 'format'>>> {
+export default class Page extends PageBlock {
 	Permissions: InstanceType<typeof NotionPermissions.Block>;
 
 	constructor (arg: INotionCoreOptions) {
 		super(arg);
 		this.Permissions = new NotionPermissions.Block(arg);
-	}
-
-	async getCachedParentData () {
-		const data = this.getCachedData();
-		return (await NotionCache.fetchDataOrReturnCached(data.parent_table, data.parent_id, this.getProps())) as
-			| ISpace
-			| IPage;
-	}
-
-	/**
-   * Add/remove this page from the favorite list
-   */
-	async updateBookmarkedStatus (favorite_status: boolean) {
-		const data = this.getCachedData();
-		let target_space_view: ISpaceView = null as any;
-		for (const [ , space_view ] of this.cache.space_view) {
-			if (space_view.space_id === data.space_id) {
-				target_space_view = space_view;
-				break;
-			}
-		}
-		await NotionOperations.executeOperations(
-			[
-				...(await NotionLineage.updateChildContainer<ISpaceView>(
-					'space_view',
-					target_space_view.id,
-					favorite_status,
-					data.id,
-					'bookmarked_pages',
-					this.getProps()
-				))
-			],
-			this.getProps()
-		);
 	}
 
 	/**
@@ -134,15 +97,5 @@ export default class Page extends Block<IPage, Partial<Pick<IPage, 'properties' 
 			(block_id) => this.cache.block.get(block_id),
 			async (id, block, container) => NotionCore.PopulateMap.block(block, container, this.getProps())
 		);
-	}
-
-	async toggleFollow () {
-		const follow_id = NotionLineage.Page.getFollowId(this.id, this.cache)!;
-		const follow = this.cache.follow.get(follow_id)!;
-		await NotionOperations.executeOperations(
-			[ NotionOperations.Chunk.follow.set(follow_id, [ 'following' ], !follow.following) ],
-			this.getProps()
-		);
-		follow.following = !follow.following;
 	}
 }

7c29d7ae63c39fe30baa2d0dcc7bc80041d1f4a0
Safwan Shaheer <devorein00@gmail.com>
2021-04-20 17:20:21 +0600

    TDS(dev): Added tds for api error and data response

--- apps/dev.to/src/index.ts
+++ apps/dev.to/src/index.ts
@@ -1,13 +1,13 @@
 import { NotionCache } from '@nishans/cache';
 import { ICollectionBlockInput, NotionFabricator } from '@nishans/fabricator';
 import axios from 'axios';
-import { ICreateBookmarkRowsParams } from './types';
+import { IBookmarksFeedResponse, ICreateBookmarkRowsParams, TApiError } from './types';
 
 export * from './types';
 
 export async function createBookmarkRows (options: ICreateBookmarkRowsParams) {
   try{
-    const response = await axios.post(
+    const response = await axios.post<TApiError | IBookmarksFeedResponse>(
       'https://api.daily.dev/graphql',
       {
         query:
@@ -24,42 +24,47 @@ export async function createBookmarkRows (options: ICreateBookmarkRowsParams) {
         }
       }
     );
-    console.log(JSON.stringify(response.data, null, 2))
-  
-    const schema: ICollectionBlockInput["schema"] = [{
-      name: "Dev.to Id",
-      type: "text",
-    }, {
-      name: "Title",
-      type: "title"
-    }];
-  
-    await NotionFabricator.CreateData.contents([{
-      type: "collection_view_page",
-      name: [[options.title ?? "Dev.to Bookmarks"]],
-      schema,
-      views: [
-        {
-          type: "table",
-          name: "Table",
-          schema_units: [{
-            name: "Title",
-            type: "title",
-          }]
-        }
-      ],
-      rows: [response.data.data.page.edges.slice(0, 5).map(edge=>({
-        title: edge.title,
-      }))]
-    }], options.parent_id, options.parent_table, {
-      cache: NotionCache.createDefaultCache(),
-      shard_id: options.shard_id,
-      token: options.notion_cookie,
-      space_id: options.space_id,
-      user_id: options.user_id
-    })
-  
-    console.log(schema);
+    if("errors" in response.data){
+      throw new Error(response.data.errors[0].message)
+    }else if("page" in response.data){
+      const schema: ICollectionBlockInput["schema"] = [{
+        name: "Dev.to Id",
+        type: "text",
+      }, {
+        name: "Title",
+        type: "title"
+      }];
+      await NotionFabricator.CreateData.contents([{
+        type: "collection_view_page",
+        name: [[options.title ?? "Dev.to Bookmarks"]],
+        schema,
+        views: [
+          {
+            type: "table",
+            name: "Table",
+            schema_units: [{
+              name: "Title",
+              type: "title",
+            }]
+          }
+        ],
+        rows: response.data.page.edges.slice(0, 5).map(edge=>({
+          type: "page",
+          properties: {
+            title: [[edge.node.title]],
+          },
+          contents: []
+        }))
+      }], options.parent_id, options.parent_table, {
+        cache: NotionCache.createDefaultCache(),
+        shard_id: options.shard_id,
+        token: options.notion_cookie,
+        space_id: options.space_id,
+        user_id: options.user_id
+      })
+    
+      console.log(schema);
+    }
   }catch(err){
     console.log(err)
   }
--- apps/dev.to/src/types.ts
+++ apps/dev.to/src/types.ts
@@ -8,3 +8,59 @@ export interface ICreateBookmarkRowsParams {
 	shard_id: number;
 	user_id: string;
 }
+
+export interface IUnauthenticatedApiError {
+	errors: {
+		message: 'Access denied! You need to be authorized to perform this action!';
+		locations: [
+			{
+				line: number;
+				column: number;
+			}
+		];
+		path: string[];
+		extensions: {
+			code: 'UNAUTHENTICATED';
+		};
+	}[];
+	data: null;
+}
+
+export type TApiError = IUnauthenticatedApiError;
+
+export interface IBookmarksFeedResponse {
+	page: {
+		pageInfo: {
+			hasNextPage: boolean;
+			endCursor: string;
+		};
+		edges: {
+			node: {
+				id: string;
+				title: string;
+				createdAt: string;
+				image: string;
+				readTime: number;
+				source: {
+					id: string;
+					name: string;
+					image: string;
+				};
+				permalink: string;
+				numComments: number;
+				numUpvotes: number;
+				commentsPermalink: string;
+				author: {
+					name: string;
+					image: string;
+				};
+				featuredComments: string[];
+				trending: null;
+				read: boolean;
+				upvoted: boolean;
+				commented: boolean;
+				bookmarked: boolean;
+			};
+		}[];
+	};
+}

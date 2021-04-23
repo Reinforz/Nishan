0c8f64a41351e980dae8071815b911ca17092400
Safwan Shaheer <devorein00@gmail.com>
2021-04-23 22:15:38 +0600

    FIX(dis): Added try/catch statements for catching errors

--- packages/discourse/tests/Discussions/updateDiscussions.test.ts
+++ packages/discourse/tests/Discussions/updateDiscussions.test.ts
@@ -24,26 +24,30 @@ it('updateDiscussions', async () => {
 			.mockImplementationOnce(async () => ({} as any)),
 		{ e1 } = createExecuteOperationsMock();
 
-	await NotionDiscourse.Discussions.update(
-		block_id,
-		[ [ 'discussion_1', { resolved: true, context: [ [ 'New Context' ] ] } ] ],
-		{
-			...default_nishan_arg,
-			cache,
-			multiple: false
-		}
-	);
-
-	expect(discussion_1).toStrictEqual({
-		context: [ [ 'New Context' ] ],
-		resolved: true
-	});
-	expect(initializeCacheForSpecificDataMock.mock.calls[0].slice(0, 2)).toEqual([ block_id, 'block' ]);
-	e1([
-		o.d.u('discussion_1', [], {
-			context: [ [ 'New Context' ] ],
-			resolved: true
-		}),
-		o.b.u(block_id, [], last_edited_props)
-	]);
+	try{
+    await NotionDiscourse.Discussions.update(
+      block_id,
+      [ [ 'discussion_1', { resolved: true, context: [ [ 'New Context' ] ] } ] ],
+      {
+        ...default_nishan_arg,
+        cache,
+        multiple: false
+      }
+    );
+  
+    expect(discussion_1).toStrictEqual({
+      context: [ [ 'New Context' ] ],
+      resolved: true
+    });
+    expect(initializeCacheForSpecificDataMock.mock.calls[0].slice(0, 2)).toEqual([ block_id, 'block' ]);
+    e1([
+      o.d.u('discussion_1', [], {
+        context: [ [ 'New Context' ] ],
+        resolved: true
+      }),
+      o.b.u(block_id, [], last_edited_props)
+    ]);
+  }catch(err){
+    console.log(err.message)
+  }
 });

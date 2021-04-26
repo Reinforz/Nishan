2d306ee823f11fa3f3dca417ad1d3585a73e2d07
Safwan Shaheer <devorein00@gmail.com>
2021-04-26 20:29:19 +0600

    TEST(dis): Added space and block to cache

--- packages/discourse/tests/Comments/deleteComments.test.ts
+++ packages/discourse/tests/Comments/deleteComments.test.ts
@@ -1,43 +1,55 @@
 import { NotionCache } from '@nishans/cache';
 import { NotionIdz } from '@nishans/idz';
 import { createExecuteOperationsMock } from '../../../../utils/tests';
-import { default_nishan_arg, last_edited_props, o } from '../../../core/tests/utils';
+import {
+  default_nishan_arg,
+  last_edited_props,
+  o
+} from '../../../core/tests/utils';
 import { NotionDiscourse } from '../../libs';
 
 afterEach(() => {
-	jest.restoreAllMocks();
+  jest.restoreAllMocks();
 });
 
 it(`NotionDiscourse.deleteComments`, async () => {
-	const comment_id = NotionIdz.Generate.id(),
-		comment_data: any = { id: comment_id, parent_id: 'discussion_1' },
-		discussion_data: any = { comments: [ comment_id ], id: 'discussion_1' },
-		cache = {
-			...NotionCache.createDefaultCache(),
-			comment: new Map([ [ comment_id, comment_data ] ]),
-			discussion: new Map([ [ 'discussion_1', discussion_data ] ])
-		},
-		options = {
-			...default_nishan_arg,
-			cache
-		},
-		initializeCacheForSpecificDataMock = jest
-			.spyOn(NotionCache, 'initializeCacheForSpecificData')
-			.mockImplementation(async () => undefined),
-		{ e1 } = createExecuteOperationsMock();
+  const comment_id = NotionIdz.Generate.id(),
+    comment_data: any = { id: comment_id, parent_id: 'discussion_1' },
+    discussion_data: any = { comments: [comment_id], id: 'discussion_1' },
+    cache = {
+      ...NotionCache.createDefaultCache(),
+      comment: new Map([[comment_id, comment_data]]),
+      discussion: new Map([['discussion_1', discussion_data]]),
+      block: new Map([
+        ['block_1', { id: 'block_1', discussions: ['discussion_1'] }]
+      ]),
+      space: new Map([['space_1', { id: 'space_1' }]]),
+      notion_user: new Map([['notion_user_1', { id: 'notion_user_1' }]])
+    } as any,
+    options = {
+      ...default_nishan_arg,
+      cache
+    },
+    initializeCacheForSpecificDataMock = jest
+      .spyOn(NotionCache, 'initializeCacheForSpecificData')
+      .mockImplementation(async () => undefined),
+    { e1 } = createExecuteOperationsMock();
 
-	await NotionDiscourse.Comments.delete('discussion_1', [ comment_id ], options);
+  await NotionDiscourse.Comments.delete('discussion_1', [comment_id], options);
 
-	e1([
-		o.cm.u(comment_id, [], {
-			alive: false,
-			...last_edited_props
-		}),
-		o.d.lr('discussion_1', [ 'comments' ], {
-			id: comment_id
-		})
-	]);
-	expect(initializeCacheForSpecificDataMock.mock.calls[0].slice(0, 2)).toEqual([ 'discussion_1', 'discussion' ]);
-	expect(comment_data.alive).toBe(false);
-	expect(discussion_data.comments).toStrictEqual([]);
+  e1([
+    o.cm.u(comment_id, [], {
+      alive: false,
+      ...last_edited_props
+    }),
+    o.d.lr('discussion_1', ['comments'], {
+      id: comment_id
+    })
+  ]);
+  expect(initializeCacheForSpecificDataMock.mock.calls[0].slice(0, 2)).toEqual([
+    'discussion_1',
+    'discussion'
+  ]);
+  expect(comment_data.alive).toBe(false);
+  expect(discussion_data.comments).toStrictEqual([]);
 });

433da554a09dfe9e5077130a6f7aecf359abb994
Safwan Shaheer <devorein00@gmail.com>
2021-04-26 19:11:03 +0600

    FIX(dis): Added try/catch block around NotionOperations.executeOperations

--- packages/traverser/libs/update.ts
+++ packages/traverser/libs/update.ts
@@ -14,63 +14,81 @@ import { getChildIds, iterateChildren } from './utils';
  * @param cb additional callback
  */
 export const update = async <T extends TData, CD, RD, C = any[]>(
-	args: UpdateTypes<CD, RD>,
-	transform: ((id: string) => CD | undefined | Promise<CD | undefined>),
-	options: IterateAndUpdateChildrenOptions<T, C>,
-	cb?: ((id: string, child_data: CD, updated_data: RD, container: C) => any)
+  args: UpdateTypes<CD, RD>,
+  transform: (id: string) => CD | undefined | Promise<CD | undefined>,
+  options: IterateAndUpdateChildrenOptions<T, C>,
+  cb?: (id: string, child_data: CD, updated_data: RD, container: C) => any
 ) => {
-	const {
-			container,
-			manual = false,
-			user_id,
-			parent_id,
-			multiple = true,
-			child_type,
-			logger,
-			cache,
-			parent_type,
-			child_ids
-		} = options,
-		parent_data = cache[parent_type].get(parent_id) as T,
-		operations: IOperation[] = [];
+  const {
+      container,
+      manual = false,
+      user_id,
+      parent_id,
+      multiple = true,
+      child_type,
+      logger,
+      cache,
+      parent_type,
+      child_ids
+    } = options,
+    parent_data = cache[parent_type].get(parent_id) as T,
+    operations: IOperation[] = [];
 
-	const iterateUtil = async (child_id: string, child_data: CD, updated_data: RD) => {
-		cb && (await cb(child_id, child_data, updated_data, container));
-		logger && NotionLogger.method.info(`UPDATE ${child_type} ${child_id}`);
+  const iterateUtil = async (
+    child_id: string,
+    child_data: CD,
+    updated_data: RD
+  ) => {
+    cb && (await cb(child_id, child_data, updated_data, container));
+    logger && NotionLogger.method.info(`UPDATE ${child_type} ${child_id}`);
 
-		let last_edited_props = {};
-		if (!manual) {
-			// If data update is not manual
-			// 1. Update the last edited props of the data
-			// 2. deeply merge the new data with the existing data
-			// 3. Push the updated properties to the stack
+    let last_edited_props = {};
+    if (!manual) {
+      // If data update is not manual
+      // 1. Update the last edited props of the data
+      // 2. deeply merge the new data with the existing data
+      // 3. Push the updated properties to the stack
 
-			if (NotionValidators.dataContainsEditedProps(child_type))
-				last_edited_props = NotionUtils.updateLastEditedProps(child_data, user_id);
+      if (NotionValidators.dataContainsEditedProps(child_type))
+        last_edited_props = NotionUtils.updateLastEditedProps(
+          child_data,
+          user_id
+        );
 
-			NotionUtils.deepMerge(child_data, updated_data);
-			operations.push(
-				NotionOperations.Chunk[child_type].update(child_id, [], { ...updated_data, ...last_edited_props })
-			);
-		}
-	};
+      NotionUtils.deepMerge(child_data, updated_data);
+      operations.push(
+        NotionOperations.Chunk[child_type].update(child_id, [], {
+          ...updated_data,
+          ...last_edited_props
+        })
+      );
+    }
+  };
 
-	await iterateChildren<CD, RD>({ args, cb: iterateUtil, method: 'UPDATE' }, transform, {
-		child_ids: getChildIds(child_ids, parent_data),
-		multiple,
-		child_type,
-		parent_id,
-		parent_type
-	});
+  await iterateChildren<CD, RD>(
+    { args, cb: iterateUtil, method: 'UPDATE' },
+    transform,
+    {
+      child_ids: getChildIds(child_ids, parent_data),
+      multiple,
+      child_type,
+      parent_id,
+      parent_type
+    }
+  );
 
-	if (NotionValidators.dataContainsEditedProps(parent_type))
-		operations.push(
-			NotionOperations.Chunk[parent_type].update(parent_id, [], NotionUtils.updateLastEditedProps(parent_data, user_id))
-		);
-	try{
+  if (NotionValidators.dataContainsEditedProps(parent_type))
+    operations.push(
+      NotionOperations.Chunk[parent_type].update(
+        parent_id,
+        [],
+        NotionUtils.updateLastEditedProps(parent_data, user_id)
+      )
+    );
+  try {
     await NotionOperations.executeOperations(operations, options);
-  }catch(err){
-    console.log("NotionOperations.executeOperations", err)
+    return container as C;
+  } catch (err) {
+    console.log('NotionOperations.executeOperations', err);
   }
-	return container as C;
 };

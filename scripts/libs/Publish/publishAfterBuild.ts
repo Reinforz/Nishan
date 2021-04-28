f4655f3d581b7b8bf652383587a87606cd2006d2
Safwan Shaheer <devorein00@gmail.com>
2021-04-28 22:58:14 +0600

    DEPS(*): Updated internal deps

--- scripts/libs/Publish/publishAfterBuild.ts
+++ scripts/libs/Publish/publishAfterBuild.ts
@@ -5,63 +5,76 @@ import path from 'path';
 import { NishanScripts } from '../';
 import { IPackageMap } from '../types';
 
-/* function sleep(time: number){
-  return new Promise((resolve)=>setTimeout(()=>resolve(true), time));
-} */
-
-export async function publishAfterBuild (packages: string[], packages_map: IPackageMap, cbs?: (package_name: string, step: string) => any) {
-	const packages_dir = path.resolve(__dirname, '../../../../packages');
+export async function publishAfterBuild(
+  packages: string[],
+  packages_map: IPackageMap,
+  cbs?: (package_name: string, step: string) => any
+) {
+  const packages_dir = path.resolve(__dirname, '../../../../packages');
   const reversed_packages = [...packages].reverse();
-  const non_builded_packages_data_path = path.resolve(__dirname, '../../../data/non_builded_packages.json');
+  const non_builded_packages_data_path = path.resolve(
+    __dirname,
+    '../../../data/non_builded_packages.json'
+  );
 
-
-	for (let index = 0; index < packages.length; index++) {
-		const package_name = packages[index].split('/')[1];
-		const package_dir = path.join(packages_dir, package_name);
-		console.log(colors.green.bold(`Building ${packages[index]}`));
+  for (let index = 0; index < packages.length; index++) {
+    const package_name = packages[index].split('/')[1];
+    const package_dir = path.join(packages_dir, package_name);
+    console.log(colors.green.bold(`Building ${packages[index]}`));
     NishanScripts.Update.patchVersion(packages[index], packages_map, 1);
-    // const non_installed_deps = await NishanScripts.Get.packageNonInstalledDependencies(package_name);
-    // if(Array.from(non_installed_deps.keys()).length !==0 ){
-    //   console.log(non_installed_deps);
-    //   throw new Error(`Uninstalled deps in @nishans/${package_name}`)
-    // }
-    // cbs && cbs(package_name, 'import_checker');
-    const {package_json_data} = (await NishanScripts.Get.packageJsonData(packages[index]));
-		try {
-      if(package_json_data?.scripts?.test){
+    const { package_json_data } = await NishanScripts.Get.packageJsonData(
+      packages[index]
+    );
+    try {
+      if (package_json_data?.scripts?.test) {
         cp.execSync(`npm run test`, { cwd: package_dir });
         console.log(colors.green.bold(`Test completed`));
         cbs && cbs(package_name, 'test');
       }
-      
-			cp.execSync(`npx del-cli ./dist`, { cwd: package_dir });
-			console.log(colors.green.bold(`Deleted dist folder`));
 
-			cp.execSync(`npm run build`, { cwd: package_dir });
-			console.log(colors.green.bold(`Regular transpile completed`));
+      cp.execSync(`npx del-cli ./dist`, { cwd: package_dir });
+      console.log(colors.green.bold(`Deleted dist folder`));
+
+      cp.execSync(`npm run build`, { cwd: package_dir });
+      console.log(colors.green.bold(`Regular transpile completed`));
 
       cbs && cbs(package_name, 'transpile');
-			cp.execSync(`tsc --sourceMap false --removeComments --declaration false`, {
-				cwd: package_dir
-			});
+      cp.execSync(
+        `tsc --sourceMap false --removeComments --declaration false`,
+        {
+          cwd: package_dir
+        }
+      );
 
-			console.log(colors.green.bold(`Nocomments transpile completed`));
+      console.log(colors.green.bold(`Nocomments transpile completed`));
       cbs && cbs(package_name, 'transpile_nocomments');
 
-      await NishanScripts.Update.packageDependency(packages[index], packages_map);
+      await NishanScripts.Update.packageDependency(
+        packages[index],
+        packages_map
+      );
       cbs && cbs(package_name, 'update_json');
 
-      cp.execSync(`npm publish --access=public`, { cwd: path.join(packages_dir, package_name) });
-			console.log(colors.green.bold(`Published ${package_name}`));
+      cp.execSync(`npm publish --access=public`, {
+        cwd: path.join(packages_dir, package_name)
+      });
+      console.log(colors.green.bold(`Published ${package_name}`));
       cbs && cbs(package_name, 'publish');
-		} catch (err) {
+    } catch (err) {
       NishanScripts.Update.patchVersion(packages[index], packages_map, -1);
-      await NishanScripts.Update.packageDependency(packages[index], packages_map);
-			console.log(colors.red.bold(`Error encountered in ${package_name}`));
-			console.log(err.message);
-      await fs.promises.writeFile(non_builded_packages_data_path, JSON.stringify(reversed_packages.reverse(), null, 2), 'utf-8');
+      await NishanScripts.Update.packageDependency(
+        packages[index],
+        packages_map
+      );
+      console.log(colors.red.bold(`Error encountered in ${package_name}`));
+      console.log(err.message);
+      await fs.promises.writeFile(
+        non_builded_packages_data_path,
+        JSON.stringify(reversed_packages.reverse(), null, 2),
+        'utf-8'
+      );
       break;
-		}
+    }
     reversed_packages.pop();
-	}
+  }
 }
--- scripts/libs/Update/updatePackageDependency.ts
+++ scripts/libs/Update/updatePackageDependency.ts
@@ -3,23 +3,44 @@ import fs from 'fs';
 import { NishanScripts } from '../';
 import { IPackageMap } from '../types';
 
-export async function updatePackageDependency (package_name: string, packages_map: IPackageMap) {
-	const { package_json_data, package_json_path } = await NishanScripts.Get.packageJsonData(package_name);
-	const package_info = packages_map.get(package_name);
-	if (package_info) {
+export async function updatePackageDependency(
+  package_name: string,
+  packages_map: IPackageMap
+) {
+  const {
+    package_json_data,
+    package_json_path
+  } = await NishanScripts.Get.packageJsonData(package_name);
+  const package_info = packages_map.get(package_name);
+  if (package_info) {
     console.log(colors.bold.blue(`${package_name} ${package_info.version}`));
     package_json_data.version = package_info.version;
   }
-  
-	[ 'dependencies', 'devDependencies' ].forEach((dependency_type) => {
-		Object.keys(package_json_data[dependency_type] ?? {}).forEach((dependency_name) => {
-			const packages_deps_info = packages_map.get(dependency_name)!;
-			if (packages_deps_info) {
-        package_json_data[dependency_type][dependency_name] = packages_deps_info.version;
-        console.log(colors.bold.blue(`${dependency_type} ${dependency_name} ${packages_deps_info.version}`));
-      }
-		});
-	});
 
-	await fs.promises.writeFile(package_json_path, JSON.stringify(package_json_data, null, 2), 'utf-8');
+  ['dependencies', 'devDependencies'].forEach((dependency_type) => {
+    Object.keys(package_json_data[dependency_type] ?? {}).forEach(
+      (dependency_name) => {
+        const packages_deps_info = packages_map.get(dependency_name)!;
+        if (packages_deps_info) {
+          package_json_data[dependency_type][dependency_name] =
+            packages_deps_info.version;
+          if (
+            package_json_data[dependency_type][dependency_name] !==
+            packages_deps_info.version
+          )
+            console.log(
+              colors.bold.blue(
+                `${dependency_type} ${dependency_name} ${packages_deps_info.version}`
+              )
+            );
+        }
+      }
+    );
+  });
+
+  await fs.promises.writeFile(
+    package_json_path,
+    JSON.stringify(package_json_data, null, 2),
+    'utf-8'
+  );
 }

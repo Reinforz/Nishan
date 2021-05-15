f5865e4a30fdff5c03cb2b2e6807fe10a6558d37
Safwan Shaheer <devorein00@gmail.com>
2021-05-15 17:59:57 +0600

    FEAT(orm): Added database already exists check when creating one

--- packages/orm/libs/Db/createDbs.ts
+++ packages/orm/libs/Db/createDbs.ts
@@ -1,9 +1,22 @@
 import { NotionCore } from '@nishans/core';
+import { NotionLogger } from '@nishans/logger';
 
 export async function createDbs(
   titles: string[],
   space: InstanceType<typeof NotionCore.Api.Space>
 ) {
+  const root_page_map = await space.getRootPage(
+    (page) =>
+      page.type === 'page' && titles.includes(page.properties.title[0][0])
+  );
+  const root_page_entries = Array.from(root_page_map.page.entries());
+
+  if (root_page_entries.length !== 0) {
+    const msg = `${
+      root_page_entries[0][1].getCachedData().properties.title[0][0]
+    } database already exists`;
+    NotionLogger.error(msg);
+  }
   const page_map = await space.createRootPages(
     titles.map((title) => ({
       type: 'page',

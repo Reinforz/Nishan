c7f90d14717c20ed016516d9b7805903a8603d0e
Safwan Shaheer <devorein00@gmail.com>
2021-08-18 17:31:48 +0600

    TD(types): Added types for SAML error and endpoint

--- packages/types/libs/endpoints.ts
+++ packages/types/libs/endpoints.ts
@@ -14,6 +14,7 @@ import {
   NotionApiUserRateLimitResponseError,
   NotionApiUserValidationIncorrectPasswordError,
   NotionApiUserValidationInvalidOrExpiredPasswordError,
+  NotionApiUserValidationSAMLError,
   NotionApiUserValidationUserWithEmailExistsError,
   RecordMap,
   SubscribedSubscriptionData,
@@ -150,6 +151,12 @@ export interface INotionEndpoints {
         }
       >;
     }
+  >;
+  getSamlRedirect: INotionEndpoint<
+    {
+      email: string;
+    },
+    Record<string, unknown> | NotionApiUserValidationSAMLError
   >;
   logoutActiveSessions: INotionEndpoint<
     Record<string, unknown>,
--- packages/types/libs/error.ts
+++ packages/types/libs/error.ts
@@ -1,113 +1,124 @@
 export type TNotionApiErrorName =
-	| 'UserValidationError'
-	| 'UnauthorizedError'
-	| 'UserRateLimitResponse'
-	| 'ValidationError'
-	| 'PostgresNullConstraintError';
+  | 'UserValidationError'
+  | 'UnauthorizedError'
+  | 'UserRateLimitResponse'
+  | 'ValidationError'
+  | 'PostgresNullConstraintError';
 
 export interface INotionApiError {
-	errorId: string;
-	name: TNotionApiErrorName;
-	message: string;
+  errorId: string;
+  name: TNotionApiErrorName;
+  message: string;
 }
 
 export type TNotionApiUserValidationError =
-	| NotionApiUserValidationPasswordNotEnteredError
-	| NotionApiUserValidationIncorrectPasswordError
-	| NotionApiUserValidationInvalidOrExpiredPasswordError
-	| NotionApiUserValidationUserWithEmailExistsError;
+  | NotionApiUserValidationPasswordNotEnteredError
+  | NotionApiUserValidationIncorrectPasswordError
+  | NotionApiUserValidationInvalidOrExpiredPasswordError
+  | NotionApiUserValidationUserWithEmailExistsError;
 
-export interface NotionApiUserValidationPasswordNotEnteredError extends INotionApiError {
-	message: 'Please enter your password.';
-	name: 'UserValidationError';
-	clientData: { type: 'password_not_entered' };
+export interface NotionApiUserValidationPasswordNotEnteredError
+  extends INotionApiError {
+  message: 'Please enter your password.';
+  name: 'UserValidationError';
+  clientData: { type: 'password_not_entered' };
 }
 
-export interface NotionApiUserValidationIncorrectPasswordError extends INotionApiError {
-	message: 'Incorrect password.';
-	name: 'UserValidationError';
-	clientData: { type: 'incorrect_password' };
+export interface NotionApiUserValidationIncorrectPasswordError
+  extends INotionApiError {
+  message: 'Incorrect password.';
+  name: 'UserValidationError';
+  clientData: { type: 'incorrect_password' };
 }
 
-export interface NotionApiUserValidationInvalidOrExpiredPasswordError extends INotionApiError {
-	message: 'Invalid or expired password.';
-	name: 'UserValidationError';
-	clientData: { type: 'invalid_or_expired_password' };
+export interface NotionApiUserValidationInvalidOrExpiredPasswordError
+  extends INotionApiError {
+  message: 'Invalid or expired password.';
+  name: 'UserValidationError';
+  clientData: { type: 'invalid_or_expired_password' };
 }
 
-export interface NotionApiUserValidationUserWithEmailExistsError extends INotionApiError {
-	message: 'A user with this email already exists.';
-	name: 'UserValidationError';
-	clientData: { type: 'user_with_email_already_exists' };
+export interface NotionApiUserValidationUserWithEmailExistsError
+  extends INotionApiError {
+  message: 'A user with this email already exists.';
+  name: 'UserValidationError';
+  clientData: { type: 'user_with_email_already_exists' };
+}
+
+export interface NotionApiUserValidationSAMLError extends INotionApiError {
+  message: 'This email is not configured to use SAML SSO';
+  name: 'UserValidationError';
+  clientData: { type: 'email_not_configured_for_saml_sso' };
 }
 
 export interface NotionApiUnauthorizedError extends INotionApiError {
-	message: 'Must be authenticated.';
-	name: 'UnauthorizedError';
+  message: 'Must be authenticated.';
+  name: 'UnauthorizedError';
 }
 
-export interface NotionApiUnauthorizedLoginTryAgainError extends INotionApiError {
-	message: 'Token was invalid or expired.';
-	name: 'UnauthorizedError';
-	clientData: { type: 'login_try_again' };
+export interface NotionApiUnauthorizedLoginTryAgainError
+  extends INotionApiError {
+  message: 'Token was invalid or expired.';
+  name: 'UnauthorizedError';
+  clientData: { type: 'login_try_again' };
 }
 
 export interface NotionApiUserRateLimitResponseError extends INotionApiError {
-	message: 'Please try again later.';
-	name: 'UserRateLimitResponse';
-	clientData: { type: 'rate_limited' };
+  message: 'Please try again later.';
+  name: 'UserRateLimitResponse';
+  clientData: { type: 'rate_limited' };
 }
 
 export interface NotionApiUnsavedTransactionError extends INotionApiError {
-	message: 'Unsaved transactions.';
-	name: 'ValidationError';
-	clientData: {
-		type: 'unsaved_transactions';
-		errors: [
-			{
-				id: string;
-				name: 'ValidationError';
-				retryable: boolean;
-			}
-		];
-		untried: string[];
-	};
+  message: 'Unsaved transactions.';
+  name: 'ValidationError';
+  clientData: {
+    type: 'unsaved_transactions';
+    errors: [
+      {
+        id: string;
+        name: 'ValidationError';
+        retryable: boolean;
+      }
+    ];
+    untried: string[];
+  };
 }
 export interface NotionApiInvalidInputError extends INotionApiError {
-	message: 'Invalid input.';
-	name: 'ValidationError';
+  message: 'Invalid input.';
+  name: 'ValidationError';
 }
 
 export interface NotionApiPostgresNullConstraintError extends INotionApiError {
-	name: 'PostgresNullConstraintError';
-	message: 'Unsaved transactions.';
-	clientData: {
-		type: 'unsaved_transactions';
-		errors: [
-			{
-				id: string;
-				name: 'PostgresNullConstraintError';
-				retryable: boolean;
-			}
-		];
-		untried: [];
-	};
+  name: 'PostgresNullConstraintError';
+  message: 'Unsaved transactions.';
+  clientData: {
+    type: 'unsaved_transactions';
+    errors: [
+      {
+        id: string;
+        name: 'PostgresNullConstraintError';
+        retryable: boolean;
+      }
+    ];
+    untried: [];
+  };
 }
 
 export interface NotionApiMissingTokenError extends INotionApiError {
-	message: string;
-	name: 'ValidationError';
+  message: string;
+  name: 'ValidationError';
 }
 
 export type TNotionApiValidatorError =
-	| NotionApiMissingTokenError
-	| NotionApiInvalidInputError
-	| NotionApiUnsavedTransactionError;
+  | NotionApiMissingTokenError
+  | NotionApiInvalidInputError
+  | NotionApiUnsavedTransactionError;
 
 export type TNotionApiError =
-	| NotionApiUserRateLimitResponseError
-	| NotionApiUnauthorizedError
-	| TNotionApiValidatorError
-	| TNotionApiUserValidationError
-	| NotionApiPostgresNullConstraintError
-	| NotionApiUnauthorizedLoginTryAgainError;
+  | NotionApiUserRateLimitResponseError
+  | NotionApiUnauthorizedError
+  | TNotionApiValidatorError
+  | TNotionApiUserValidationError
+  | NotionApiPostgresNullConstraintError
+  | NotionApiUnauthorizedLoginTryAgainError;
